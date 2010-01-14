33b351a01fa59a99cfeb62211e8d2300b359380a
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-01-14 12:00:38 +0000

    Filling up interface ISorterAdapter (missed that) - and let get_url_forsort() return an Url instance

--- trunk/gyro/core/lib/interfaces/isorteradapter.cls.php
+++ trunk/gyro/core/lib/interfaces/isorteradapter.cls.php
@@ -8,5 +8,28 @@
  * @ingroup Interfaces
  */
 interface ISorterAdapter {
-	
+	/**
+	 * Return column key of column to sort after
+	 * 
+	 * @return string
+	 */
+	public function get_column();
+
+	/**
+	 * Returns sort order ('forward' or 'backward')
+	 * 
+	 * False if no order was set 
+	 * 
+	 * @return string
+	 */
+	public function get_order();
+
+	/**
+	 * Return link to invoke sorting on given column with given order
+	 *
+	 * @param string $column
+	 * @param string $order
+	 * @return Url
+	 */
+	public function get_url_for_sort($column, $order);
 }
