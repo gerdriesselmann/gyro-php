d8ef738d005e996efc4357cb6e995ac9cab04595
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-01-14 10:39:52 +0000

    Sphinx will return 1000 results only, even if it finds more. This must be considered when building pagers. Added a helper function for that.

--- trunk/contributions/sphinx/model/base/dataobjectsphinx.base.cls.php
+++ trunk/contributions/sphinx/model/base/dataobjectsphinx.base.cls.php
@@ -40,4 +40,23 @@ class DataObjectSphinxBase extends DataObjectBase {
 		$cmd = new SphinxIndexRotateCommand($this);
 		return $cmd->execute(); 	
 	}
+	
+	/**
+	 * Return a count suitable for pagers. 
+	 *  
+	 * Sphinx will return a limited number of results, usually 1,000, regardless
+	 * of the number of matches. This is defined by the configuration setting 
+	 * "max_matches" in the server config file.
+	 * 
+	 * This means limiting a result to - say - items 1,100 to 1,110 will result in 
+	 * an error, and due to the Gyro way of dealing with DB error will raise an exception.
+	 * 
+	 * Therefor when creating a pager instance, use this function rather than the
+	 * usual count() function. 
+	 *  
+	 * @return int
+	 */
+	public function count_pager() {
+		return min($this->count(), APP_SPHINX_MAX_MATCHES);
+	}
 }
