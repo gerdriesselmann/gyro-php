03776c6fbfcd5eb14a69a13022b4fca0a39b6c00
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-01-26 20:41:34 +0000

    Adding jQuery 1.4 - and offering a config setting to switch between 1.3 and 1.4

--- trunk/contributions/javascript.jquery/install/check_preconditions.php
+++ trunk/contributions/javascript.jquery/install/check_preconditions.php
@@ -2,7 +2,7 @@
 function javascript_jquery_check_preconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('javascript.jquery') . 'www/';
+	$root = Load::get_module_dir('javascript.jquery') . 'www/' . Config::get_value(ConfigJQuery::JQUERY_VERSION) . '/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js'), SystemUpdateInstaller::COPY_OVERWRITE));
 	return $ret;
 }
