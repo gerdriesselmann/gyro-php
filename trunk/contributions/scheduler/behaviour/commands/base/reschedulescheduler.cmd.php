645665498b9c4cd126b47e6b1928aa146253ceee
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-01-28 15:52:31 +0000

    Refactoring to allow fine grain overloading

--- trunk/contributions/scheduler/behaviour/commands/base/reschedulescheduler.cmd.php
+++ trunk/contributions/scheduler/behaviour/commands/base/reschedulescheduler.cmd.php
@@ -13,23 +13,165 @@ class RescheduleSchedulerBaseCommand extends CommandChain {
 		$task = $this->get_instance();
 		/* @var $err Status */
 		$err = $this->get_params();
-		$policy = '';
-		$new_status = '';
 		
 		if ($err->is_ok()) {
-			$policy = $task->reschedule_success;
-			$new_status = Scheduler::STATUS_ACTIVE;
+			$ret->merge($this->reschedule_success($task, $err));
 		}
 		else {
-			$policy = $task->reschedule_error;
-			$new_status = Scheduler::STATUS_RESCHEDULED;
+			$ret->merge($this->reschedule_error($task, $err));
 		}
+		
+		return $ret;
+	} 
+	
+	/**
+	 * Reschedule on success
+	 * 
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param Status $err The result of task's last run
+	 * 
+	 * @return Status
+	 */
+	protected function reschedule_success(DAOScheduler $task, Status $err) {
+		$ret = new Status();
+		
+		$policy = $task->reschedule_success;
+		$params = $this->success_get_params($task, $err);
+		
+		if ($this->do_reschedule($task, $policy, $params, Scheduler::STATUS_ACTIVE, $err)) {
+			$ret->merge($this->success_on_rescheduled($task, $params, $err));
+		}
+		else {
+			$ret->merge($this->success_on_finished($task, $params, $err));
+		}
+		
+		return $ret;		
+	}
+	
+	/**
+	 * Return properties of rescheduled tasks
+	 * 
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param Status $err The result of task's last run
+	 * 
+	 * @return array
+	 */
+	protected function success_get_params(DAOScheduler $task, Status $err) {
+		return array(
+			'runs_success' => $task->runs_success + 1,
+			'runs_error' => 0,
+			'error_message' => '',
+		);				
+	}
 
-		$params = array(
-			'runs_success' => $err->is_error() ? $task->runs_success : $task->runs_success + 1,
-			'runs_error' => $err->is_error() ? $task->runs_error + 1 : 0,
-			'error_message' => $err->is_error() ? $err->message : '',
-		);
+	/**
+	 * Called if task was successfull and rescheduled
+	 *  
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param array $params Params for updateing task
+	 * @param Status $err The result of the task
+	 * 
+	 * @return Status  
+	 */
+	protected function success_on_rescheduled(DAOScheduler $task, $params, Status $err) {
+		return new Status();
+	}
+	
+	/**
+	 * Called if task was successfull and does not get rescheduled any more
+	 *  
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param array $params Params for updateing task
+	 * @param Status $err The result of the task
+	 * 
+	 * @return Status  
+	 */
+	protected function success_on_finished(DAOScheduler $task, $params, Status $err) {
+		$this->append(CommandsFactory::create_command($task, 'delete', false));
+		return new Status();
+	}
+	
+	/**
+	 * Reschedule on error
+	 * 
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param Status The result of task's last run
+	 * 
+	 * @return Status
+	 */
+	protected function reschedule_error(DAOScheduler $task, Status $err) {
+		$ret = new Status();
+		
+		$policy = $task->reschedule_error;
+		$params = $this->error_get_params($task, $err);
+		
+		if ($this->do_reschedule($task, $policy, $params, Scheduler::STATUS_RESCHEDULED, $err)) {
+			$ret->merge($this->error_on_rescheduled($task, $params, $err));
+		}
+		else {
+			$ret->merge($this->error_on_give_up($task, $params, $err));
+		}
+		
+		return $ret;
+	}
+	
+	/**
+	 * Return properties of rescheduled tasks
+	 * 
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param Status $err The result of task's last run
+	 * 
+	 * @return array
+	 */
+	protected function error_get_params(DAOScheduler $task, Status $err) {
+		return array(
+			'runs_success' => $task->runs_success,
+			'runs_error' => $task->runs_error + 1,
+			'error_message' => $err->message,
+		);		
+	}
+	
+	/**
+	 * Called if task was not successfull and rescheduled
+	 *  
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param array $params Params for updateing task
+	 * @param Status $err The result of the task
+	 * 
+	 * @return Status  
+	 */
+	protected function error_on_rescheduled(DAOScheduler $task, $params, Status $err) {
+		return new Status();
+	}
+	
+	/**
+	 * Called if task was not successfull and does not get rescheduled any more
+	 *  
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param array $params Params for updateing task
+	 * @param Status $err The result of the task
+	 * 
+	 * @return Status  
+	 */
+	protected function error_on_give_up(DAOScheduler $task, $params, Status $err) {
+		$this->append(CommandsFactory::create_command($task, 'update', $params));
+		$this->append(CommandsFactory::create_command($task, 'status', Scheduler::STATUS_ERROR));
+		return new Status();
+	}	
+	
+	/**
+	 * Actually perform rescheduling
+	 * 
+	 * @param DAOScheduler $task The task to reschedule
+	 * @param string $policy Reschedule policy. Must map to a file and class in behaviour/scheduler
+	 * @param array $params Params for updateing task
+	 * @param string $new_status Either ACTIVE or RESCHEDULED
+	 * @param Status $err The result of the task
+	 * 
+	 * @return bool TRUE if task was rescheduled. FALSE if task has had its last run and no rescheduling was done  
+	 */
+	protected function do_reschedule(DAOScheduler $task, $policy, $params, $new_status, Status $err) {
+		$ret = false;
 		
 		$policy = strtolower($policy);
 		$file = 'behaviour/scheduler/' . $policy . '.rescheduler.php';
@@ -43,20 +185,12 @@ class RescheduleSchedulerBaseCommand extends CommandChain {
 			$params['scheduledate'] = $newdate;
 			$this->append(CommandsFactory::create_command($task, 'update', $params));
 			$this->append(CommandsFactory::create_command($task, 'status', $new_status));
+			
+			$ret = true;
 		}
-		else {
-			// No rescheduling
-			if ($err->is_ok()) {
-				$this->append(CommandsFactory::create_command($task, 'delete', false));
-			}
-			else {
-				$this->append(CommandsFactory::create_command($task, 'update', $params));
-				$this->append(CommandsFactory::create_command($task, 'status', Scheduler::STATUS_ERROR));
-			}
-		}
-		
+
 		return $ret;
-	} 
+	}
 	
 	/**
 	 * Returns title of command.
