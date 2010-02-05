99df7e59cb830c79ad45cbc9109e7a44646ca1ea
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-02-05 21:40:10 +0000

    Ommit history test, if session is not available

--- trunk/gyro/modules/simpletest/simpletests/history.test.php
+++ trunk/gyro/modules/simpletest/simpletests/history.test.php
@@ -8,6 +8,7 @@
 class HistoryTest extends GyroUnitTestCase {
 	
 	public function setUp() {
+		Session::start();
 		Session::pull('history');
 	} 
 
@@ -22,41 +23,43 @@ class HistoryTest extends GyroUnitTestCase {
 		
 		$url = History::get(-1, 'http://www.example.org/test.html');
 		$this->assertIsA($url, Url);
-		$this->assertEqual('http://www.example.org/test.html', $url->build());		
+		$this->assertEqual('http://www.example.org/test.html', $url->build());
 	}
 	
 	public function test_get_filled() {
-		History::push(Url::create('http://www.example.org/1.html'));
-		History::push(Url::create('http://www.example.org/2.html'));
-
-		$url = History::get(0);
-		$this->assertIsA($url, Url);
-		$this->assertEqual('http://www.example.org/2.html', $url->build());		
-
-		$url = History::get(1);
-		$this->assertIsA($url, Url);
-		$this->assertEqual('http://www.example.org/1.html', $url->build());		
-		
-		$url = History::get(2);
-		$this->assertIsA($url, Url);
-		$this->assertEqual(Url::create(Config::get_url(Config::URL_DEFAULT_PAGE))->build(), $url->build());
-		
-		$url = History::get(3, Url::current());
-		$this->assertIsA($url, Url);
-		$this->assertEqual(Url::current()->build(), $url->build());
-		
-		$url = History::get(-1, 'http://www.example.org/test.html');
-		$this->assertIsA($url, Url);
-		$this->assertEqual('http://www.example.org/test.html', $url->build());
-
-		// Check if history respects limits		
-		for ($i = 0; $i < HISTORY_NUMBER_OF_ITEMS; $i++) {
-			History::push(Url::create('http://www.example.org/flood.html'));
+		if (Session::is_started()) {
+			History::push(Url::create('http://www.example.org/1.html'));
+			History::push(Url::create('http://www.example.org/2.html'));
+	
+			$url = History::get(0);
+			$this->assertIsA($url, Url);
+			$this->assertEqual('http://www.example.org/2.html', $url->build());		
+	
+			$url = History::get(1);
+			$this->assertIsA($url, Url);
+			$this->assertEqual('http://www.example.org/1.html', $url->build());		
+			
+			$url = History::get(2);
+			$this->assertIsA($url, Url);
+			$this->assertEqual(Url::create(Config::get_url(Config::URL_DEFAULT_PAGE))->build(), $url->build());
+			
+			$url = History::get(3, Url::current());
+			$this->assertIsA($url, Url);
+			$this->assertEqual(Url::current()->build(), $url->build());
+			
+			$url = History::get(-1, 'http://www.example.org/test.html');
+			$this->assertIsA($url, Url);
+			$this->assertEqual('http://www.example.org/test.html', $url->build());
+	
+			// Check if history respects limits		
+			for ($i = 0; $i < HISTORY_NUMBER_OF_ITEMS; $i++) {
+				History::push(Url::create('http://www.example.org/flood.html'));
+			}
+			
+			$url = History::get(HISTORY_NUMBER_OF_ITEMS);
+			$this->assertIsA($url, Url);
+			$this->assertEqual(Url::create(Config::get_url(Config::URL_DEFAULT_PAGE))->build(), $url->build());
 		}
-		
-		$url = History::get(HISTORY_NUMBER_OF_ITEMS);
-		$this->assertIsA($url, Url);
-		$this->assertEqual(Url::create(Config::get_url(Config::URL_DEFAULT_PAGE))->build(), $url->build());
 	}
 }
 ?>
