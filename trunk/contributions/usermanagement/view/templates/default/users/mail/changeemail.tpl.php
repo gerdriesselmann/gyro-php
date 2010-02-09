9046ab367a4a99165731edde434998e05221607c
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-02-09 20:31:33 +0000

    {$to} must be from the very old days - smells like smarty.

--- trunk/contributions/usermanagement/view/templates/default/users/mail/changeemail.tpl.php
+++ trunk/contributions/usermanagement/view/templates/default/users/mail/changeemail.tpl.php
@@ -4,15 +4,17 @@ You requested to change your e-mail address for your account on <?php print $app
 
 Your new e-mail will be  
 
-{$to}
+<?php print $confirmation->data; ?>
+
 
 Please visit
 
 <?php print ActionMapper::get_url('confirm', $confirmation); ?>
 
+
 to confirm the new address.
 
-Please note that the web site above is valid only for 24 hours.
+Please note that the url above is valid only for 24 hours.
 
 Best regards,
 Team <?php print $appname?>
