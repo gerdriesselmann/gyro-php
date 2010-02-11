62b23f58781d78b3e2ce1570b57b46b861552cfe
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-02-11 20:36:20 +0000

    Adding initial Tristate module

---
+++ trunk/contributions/db.tristate/model/base/fields/dbfield.tristate.cls.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * An tristate enum field
+ * 
+ * Field in DB should be defined as ENUM('FALSE','TRUE','UNKNOWN').
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup Tristate
+ */
+class DBFieldTristate extends DBFieldEnum {
+	public function __construct($name, $default_value = Tristate::UNKOWN, $policy = self::NOT_NULL) {
+		parent::__construct($name, array_keys(Tristate::get_states()), $default_value, $policy);
+	}
+}
