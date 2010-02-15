65652d0c69fb0b6e56b11e2b76bf4f2ec82f41ab
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-02-15 21:22:14 +0000

    Free text filter on name and email, not only name.

---
+++ contributions/usermanagement/controller/tools/filterusername.cls.php
@@ -0,0 +1,29 @@
+<?php
+class FilterUsername extends FilterText {
+	/**
+	 * Contructor
+	 * 
+	 * @param PageData $page_data;
+	 */
+	public function __construct($page_data, $adapter = false) {
+		$this->adapter = ($adapter instanceof IFilterTextAdapter) ? $adapter : new FilterTextDefaultAdapter($page_data, String::plain_ascii($filtername));
+		$this->page_data = $page_data;
+		$this->filter_object = new DBFilterMultiColumn(
+			array(
+				new DBFilterMultiColumnItem('name', $this->adapter->get_value(), DBWhere::OP_LIKE, DBWhere::LOGIC_OR),
+				new DBFilterMultiColumnItem('email', $this->adapter->get_value(), DBWhere::OP_LIKE, DBWhere::LOGIC_OR),
+			),
+			tr('name') 
+		);
+	}
+	
+	/**
+	 * Return current value to be filter after
+	 * 
+	 * @return string
+	 */
+	public function get_filter_value() {
+		$items = $this->filter_object->get_items(); 
+		return $items[0]->value;
+	}	
+}
