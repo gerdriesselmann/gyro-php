189cdf63e7792c22918b6add8ce9720334bf04f2
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-02-15 18:12:23 +0000

    Since it may confuse users to be just normally logged in on "lost password", redirect them to the account settings page right away.

--- contributions/usermanagement/behaviour/confirmationhandlers/onetimelogin.confirmationhandler.php
+++ contributions/usermanagement/behaviour/confirmationhandlers/onetimelogin.confirmationhandler.php
@@ -21,7 +21,7 @@ class OnetimeloginConfirmationHandler extends ConfirmationHandlerBase {
 				if (Users::do_login($user)) {
 					$msg = new Message(tr('You have been automatically logged in. You now can change your password.', 'users'));
 					$msg->persist();
-					$redirect = APP_DEFAULT_PAGE_USER;
+					$redirect = ActionMapper::get_url('edit_self', $user); // APP_DEFAULT_PAGE_USER;
 					Url::create($redirect)->redirect();
 					exit;					
 				}
