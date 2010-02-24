c44010732389b1e41f51a4795bd54eb07cd29c91
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-02-24 12:58:06 +0000

    Adding a route handler type

---
+++ contributions/text.unidecode/controller/base/routes/parameterizedroutehandlers/unidecodestring.handler.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Handle ansi unidecoded string (the "spu" type)
+ * 
+ * @since 0.5.1
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup Unidecode
+ */
+class UnidecodestringParameterizedRouteHandler implements IParameterizedRouteHandler {
+	/**
+	 * Returns the key that is used to identify this handler in route declaration, e.g. "s" or "ui>"
+	 * 
+	 * @return string
+	 */
+	public function get_type_key() {
+		return "spu";
+	}
+	
+	/**
+	 * Return regex to validate path  
+	 */
+	public function get_validate_regex($params) {
+		if ($params === false) {
+ 			return '[a-zA-Z0-9_-]*?';
+ 		}
+ 		else {
+ 			return '[a-zA-Z0-9_-]{' . preg_quote($params) . '}';
+ 		}
+	}
+	
+	/**
+	 * Preprocess a value before URL is build
+	 */
+	public function preprocess_build_url($value) {
+		return String::plain_ascii(ConverterFactory::encode($value, CONVERTER_UNIDECODE), '-', true);
+	}
+}
