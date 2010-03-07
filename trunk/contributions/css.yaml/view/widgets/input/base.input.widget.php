1af0c121d0573229a9137d8bc27633e9ac21054d
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-03-07 19:24:23 +0000

    Updated YAML to 3.2.1 and added YAML forms as a feature

---
+++ trunk/contributions/css.yaml/view/widgets/input/base.input.widget.php
@@ -0,0 +1,38 @@
+<?php
+require_once GYRO_CORE_DIR . 'view/widgets/input/base/base.input.widget.php';
+
+/**
+ * Basic input widget
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup View
+ */
+class InputWidgetBase extends InputWidgetBaseBase {
+	/**
+	 * Last steps
+	 */
+	protected function render_postprocess($output, $policy) {
+		if (!Config::has_feature(ConfigYAML::YAML_USE_FORMS)) {
+			return parent::render_postprocess($output, $policy);
+		}
+		$cls = strtolower(substr(get_class($this), 11));
+		$type = $cls;
+		switch ($cls) {
+			case 'textarea':
+			case 'password':
+			case 'file':
+				$type = 'text';
+				break;
+			case 'radio':
+			case 'checkbox':
+				$type = 'check';
+				break;
+			case 'submit':
+				$type = 'button';
+				break;			
+		}
+		
+		$ret = html::div($output, "type-$type");
+		return $ret;
+	}	
+}
