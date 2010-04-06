284d3e21df9ba6894fb772bdc982e8e4895b1c00
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-04-06 11:52:19 +0000

    Move icons etc from www to data dir

--- contributions/usermanagement.notifications/install/check_preconditions.php
+++ contributions/usermanagement.notifications/install/check_preconditions.php
@@ -2,7 +2,7 @@
 function usermanagement_notifications_check_preconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('usermanagement.notifications') . 'www/';
+	$root = Load::get_module_dir('usermanagement.notifications') . 'data/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js', 'css'), SystemUpdateInstaller::COPY_OVERWRITE));	
 	return $ret;
 }
