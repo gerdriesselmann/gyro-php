8007684f672fb0c0d60e1c28a8adaf686c64ffd3
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-04-06 11:47:09 +0000

    Adding ETag Render Decorator to allow 304 on pages not-cached.

---
+++ gyro/core/controller/base/renderdecorators/etagrenderdecorator.cls.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The etag render decorator creates a md5 hash of rendered page and compares this with an etag sended
+ * along by the browser
+ * 
+ * It does this only if no etag is already present, this is: If the site is not cached.  
+ */
+class ETagRenderDecorator extends RenderDecoratorBase {
+	/**
+	 * Render page
+	 *
+	 * @param PageData $page_data
+	 * @param IRenderDecorator Decorator to invoke render_content upon
+	 * @param int $policy If set to IView::DISPLAY, content is printed, if false it is returned only
+	 * @return void
+	 */
+	public function render_page($page_data, $content_render_decorator, $policy = IView::NONE) {
+		$ret = parent::render_page($page_data, $content_render_decorator, $policy);
+		Load::components('logger');
+		if ($ret && !Common::is_header_sent('etag')) {
+			$etag = md5($ret);
+			Logger::log('etag', $_SERVER);
+			Common::check_if_none_match($etag);
+			Common::header('etag', $etag);
+			// Send Cache headers
+			Common::header('Pragma', '', true);
+			Common::header('Cache-Control', 'private, max-age=0, pre-check=0, must-revalidate', true);
+		}
+		return $ret;
+	}	
+}
