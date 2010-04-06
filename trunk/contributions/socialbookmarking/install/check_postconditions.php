c4eebd268dbab7c924c4a8f415c1a72c2af61398
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-06 10:10:02 +0000

    MOve icons etc from www to data dir

--- trunk/contributions/socialbookmarking/install/check_postconditions.php
+++ trunk/contributions/socialbookmarking/install/check_postconditions.php
@@ -2,7 +2,7 @@
 function socialbookmarking_check_postconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('socialbookmarking') . 'www/';
+	$root = Load::get_module_dir('socialbookmarking') . 'data/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('images', 'css'), SystemUpdateInstaller::COPY_OVERWRITE));	
 	return $ret;
 }
