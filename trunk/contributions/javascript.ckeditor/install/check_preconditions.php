01317c9a97e1a1415e6dd0dd7938d5dcad76ae89
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-09 12:04:31 +0000

    Adding CK Editor module

---
+++ trunk/contributions/javascript.ckeditor/install/check_preconditions.php
@@ -0,0 +1,8 @@
+<?php
+function javascript_ckeditor_check_preconditions() {
+	$ret = new Status();
+	Load::components('systemupdateinstaller');
+	$root = Load::get_module_dir('javascript.ckeditor') . 'data/3.2/';
+	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js'), SystemUpdateInstaller::COPY_OVERWRITE));
+	return $ret;
+}
