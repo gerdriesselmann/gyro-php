393f354957b66302fe070d3ff386711bed0eec41
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-11 09:11:11 +0000

    Adding JCSS Manager code and Bugfixses for tidy and fullscreen module

---
+++ trunk/contributions/javascript.wymeditor/start.inc.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @defgroup WYMEditor
+ * @ingroup Javascript
+ * 
+ * Include WYMEditor (http://www.wymeditor.org/) 
+ *
+ * @section Usage
+ *
+ * On install, the module copies wymeditor files to the a folder named "js/wymeditor" below web root. The version
+ * of WYMEditor installed is 0.5, but it contains bugfixes for tidy and fullscreen plugin.
+ * 
+ * @see http://forum.wymeditor.org/forum/viewtopic.php?f=3&t=734
+ * 
+ * WYMEditor files are not included by default. To enable WYMEditor on a page, place the
+ * following code
+ * 
+ * @code
+ * Load::components('wymeditor');
+ * WYMEditor::enable($page_data);
+ * @endcode
+ * 
+ * This will turn all textareas with class "rte" into a WYMEditor instance.
+ * 
+ * @note It is good practice to use "rte" as classname for rich text editors.
+ * 
+ * @attention 
+ *   The above code only works if javascript.jquery module is enabled, too. If
+ *   you don't user jQuery, you must create you own configuration.   
+ * 
+ * @section Config Creating Configurations
+ * 
+ * If the default configuration is not sufficent, you may create your own. This is
+ * a two step process:
+ * 
+ * - Create and name a config
+ * - Refer to it by name when enabling editor
+ * 
+ * First, create a config:
+ * 
+ * @code
+ * Load::components('wymeditor');
+ * $config WYMEditor::create_config('fancy');
+ * $config->init_file = 'js/fancy_wym.js'; // JS script to fire up editor
+ * // Add tidy plugin
+ * $config->plugins['js/wymeditor/plugins/tidy/jquery.wymeditor.tidy.js'] = 'var wymtidy = wym.tidy();wymtidy.init();';
+ * @endcode
+ * 
+ * You now can use the config anywhere:
+ * 
+ * @code
+ * WYMEditor::enable($page_data, 'fancy);
+ * @endcode
+ *
+ * Note you don't need to change the init file, if you add or remove plugins.
+ *
+ * @section Notes Additional notes
+ *
+ * WYMEditor is released under GPL and MIT license.
+ * 
+ */
+
+EventSource::Instance()->register(new JavascriptWYMEditorEventSink());
