2b2554cb09fc5f8adcfc0ae34858d7638e7a70b0
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-11 09:13:15 +0000

    Adding dokcumentation and license

--- trunk/contributions/javascript.ckeditor/start.inc.php
+++ trunk/contributions/javascript.ckeditor/start.inc.php
@@ -7,21 +7,45 @@
  *
  * @section Usage
  *
- * On install, the module copies jquery.js to the a folder named "js" below web root. This file will
- * get included on all pages, if JCSSManager WidgetJCSS is used. It also will be compressed by JCSSManager, 
- * if compression is enabled.
+ * On install, the module copies CKEditor files to the a folder named "js/ckeditor" below web root. The version
+ * of CKEditor installed is 3.2.
  * 
- * To define the version of JQuery to use, you must specify a version to use by defining
- * the constant APP_JQUERY_VERSION. Available values are "1.3" for JQuery 1.3.2, and "1.4" for JQuery 1.4.2. 
- * The default is "1.3".
+ * CKEditor files are not included by default. To enable CKEditor on a page, place the following code
  * 
  * @code
- * define('APP_JQUERY_VERSION', '1.4');
+ * Load::components('ckeditor');
+ * CKEditor::enable($page_data);
+ * @endcode
+ * 
+ * This will turn all textareas with class "rte" into a CKEditor instance.
+ * 
+ * @note It is good practice to use "rte" as classname for rich text editors.
+ * 
+ * @section Config Creating Configurations
+ * 
+ * If the default configuration is not sufficent, you may create your own. This is
+ * a two step process:
+ * 
+ * - Create and name a config
+ * - Refer to it by name when enabling editor
+ * 
+ * First, create a config:
+ * 
+ * @code
+ * Load::components('ckeditor');
+ * $config CKEditor::create_config('fancy');
+ * $config->init_file = 'js/fancy_ck.js'; // JS script to fire up editor
+ * @endcode
+ * 
+ * You now can use the config anywhere:
+ * 
+ * @code
+ * CKEditor::enable($page_data, 'fancy);
  * @endcode
  *
  * @section Notes Additional notes
  *
- * JQuery is released under MIT license.
+ * CKEditor is released under GPL, LGPL and MPL license.
  */
-
+ 
 EventSource::Instance()->register(new JavascriptCKEditorEventSink());
