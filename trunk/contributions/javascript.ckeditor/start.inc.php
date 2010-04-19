f4d0888b89b194ea758928fe68dbee565d8f2e2f
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-19 08:55:17 +0000

    Upgraded to CKEditor 3.2.1

--- trunk/contributions/javascript.ckeditor/start.inc.php
+++ trunk/contributions/javascript.ckeditor/start.inc.php
@@ -8,7 +8,7 @@
  * @section Usage
  *
  * On install, the module copies CKEditor files to the a folder named "js/ckeditor" below web root. The version
- * of CKEditor installed is 3.2.
+ * of CKEditor installed is 3.2.1.
  * 
  * CKEditor files are not included by default. To enable CKEditor on a page, place the following code
  * 
