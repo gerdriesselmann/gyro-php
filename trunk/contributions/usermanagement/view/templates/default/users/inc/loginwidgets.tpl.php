3a903f35cad530e0cbf653cbb4a7f0befedf2526
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-04-21 09:14:45 +0000

    Seperate login and signup wiodgets from login and signup form

---
+++ trunk/contributions/usermanagement/view/templates/default/users/inc/loginwidgets.tpl.php
@@ -0,0 +1,13 @@
+<?php print $form_validation; ?>
+
+<?php print WidgetInput::output('name', tr('Username:', 'users'), $form_data); ?>
+<?php print WidgetInput::output('password', tr('Password:', 'users'), $form_data, WidgetInput::PASSWORD); ?>
+
+<?php if ($pwd_url = ActionMapper::get_path('lost_password')): ?>
+<p><a href="<?=$pwd_url?>"><?=tr('Forgot password?', 'users')?></a></p>
+<?php endif; ?>
+<?php if ($resend_url = ActionMapper::get_path('resend_registration_mail')): ?>
+<p><a href="<?=$resend_url?>"><?=tr('Registered, but got no confirmation mail?', 'users')?></a></p>
+<?php endif; ?>
+<br />		
+<?php print WidgetInput::output('stayloggedin', tr('Stay loged in.', 'users'), $form_data, WidgetInput::CHECKBOX); ?> 
