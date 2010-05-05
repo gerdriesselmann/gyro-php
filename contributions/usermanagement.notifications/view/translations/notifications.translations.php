1d5d1b01196afee6fe3125d10153745bf5dd4576
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-05-05 11:19:38 +0000

    Add translations

---
+++ contributions/usermanagement.notifications/view/translations/notifications.translations.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Notification translations
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup Notifications
+ */
+function notifications_load_translations($languages) {
+	return array(
+		'all' => array(
+			'en' => 'All',
+			'de' => 'Alle'
+		),
+		'app' => array(
+			'en' => 'Application',
+			'de' => 'Anwendung'
+		),
+		'Your %appname Notifications' => array(
+			'de' => 'Benachrichtigungen %appname'
+		),
+		'Your settings have been saved' => array(
+			'de' => 'Ihre Einstellungen wurden gespeichert'
+		),
+		'Your notifications' => array(
+			'de' => 'Ihre Benachrichtigungen'
+		),
+		'Notification Settings' => array(
+			'de' => 'Benachrichtigungs-Einstellungen'
+		),
+		'Settings' => array(
+			'de' => 'Einstellungen'
+		),
+		'Immediate e-mail' => array(
+			'de' => 'Sofortige E-Mail'
+		),
+		'Enable' => array(
+			'de' => 'Aktiviert'
+		),
+		'Choose sources' => array(
+			'de' => 'Quellen auswählen'
+		),
+		'E-mail digest' => array(
+			'de' => 'E-Mail-Zusammenfassung'
+		),
+		'Feed' => array(
+			'de' => 'RSS-Feed'
+		),
+		'Your feed url is %url' => array(
+			'de' => 'Ihre Feed-Adresse lautet %url'
+		),
+		'Save' => array(
+			'de' => 'Speichern'
+		),
+		'Show all' => array(
+			'de' => 'Alle anzeigen'
+		),
+		'Last <strong>%num</strong> out of <strong>%total</strong> unread messages.' => array(
+			'de' => '<strong>%num</strong> von <strong>%total</strong> ungelesenen Benachrichtigungen.'
+		),
+		'Show more' => array(
+			'de' => 'Mehr anzeigen'
+		),
+		'You have no unread messages.' => array(
+			'de' => 'Sie haben keine ungelesenen Benachrichtigungen.'
+		), 
+	);
+}
+
