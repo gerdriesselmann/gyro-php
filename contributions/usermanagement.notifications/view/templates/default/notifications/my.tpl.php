e26d1e7f92e8845c3e3e0150bbe1c38c3f503889
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-05-05 11:33:51 +0000

    Add link to settings

--- contributions/usermanagement.notifications/view/templates/default/notifications/my.tpl.php
+++ contributions/usermanagement.notifications/view/templates/default/notifications/my.tpl.php
@@ -5,6 +5,12 @@ $page_data->breadcrumb = WidgetBreadcrumb::output($title);
 ?>
 <h1><?=$title?></h1>
 
+<?php
+print WidgetItemMenu::output(
+	array(new ActionBase('', 'notifications_settings', tr('Notification Settings')))
+);
+?>
+
 <?php print WidgetFilter::output($filter_data)?>
 <?php gyro_include_template('notifications/inc/list')?>
 <?php print WidgetPager::output($pager_data)?>
