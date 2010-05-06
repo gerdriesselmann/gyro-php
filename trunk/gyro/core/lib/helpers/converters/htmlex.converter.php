c1b9ce259057c7abe4d2f1ba2416f30530909ce5
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-05-06 11:16:55 +0000

    When converting HTML to plain text, keep links

--- trunk/gyro/core/lib/helpers/converters/htmlex.converter.php
+++ trunk/gyro/core/lib/helpers/converters/htmlex.converter.php
@@ -26,14 +26,51 @@ class ConverterHtmlEx extends ConverterHtml {
 		}
 	}
 	
+	/**
+	 * Smarter decoding og html into plain text than done by HTML Converter
+	 * 
+	 * Keeps Links
+	 */
 	public function decode($value, $params = false) {
 		$value = str_replace("\n", ' ', $value);
 		$value = str_replace("\r", ' ', $value);
-		$value = str_replace('</p>', "</p>\n", $value);
+		// Process Links
+		$value = $this->relative_to_absolute($value, Config::get_url(Config::URL_BASEURL));
+		// Turn <a href="xxx">Text</a> into Text: xxx
+		$value = String::preg_replace('|<a.*?href="(.*?)".*?>(.*?)</a>|', '$2: $1', $value);
+		
+		$value = str_replace('</p>', "</p>\n\n", $value);
 		$value = preg_replace('@<br.*?>@', "\n", $value);
 		$value = String::unescape($value);
 		$value = strip_tags($value);
 		$value = String::preg_replace('| +|', ' ', $value);
 		return $value;		
-	} 		
+	}
+
+	/**
+	 * Helper. Turn relative URLs in text to an absolute one
+	 * 
+	 * @param string $text Text containing URLs
+	 * @param string $base baseurl
+	 */
+	protected function relative_to_absolute($text, $base) {
+		if (empty($base)) {
+			return $text;
+		}
+			
+		if (substr($base, -1, 1) != "/") { $base .= "/"; }
+		$domain = Url::create($base)->clear_query()->set_path('')->build(Url::ABSOLUTE);
+		
+		// Replace href="/abc" with domain
+		$pattern = 	'|<a(.*?) href="/(.*?)"|';
+		$replace = '<a$1 href="' . $domain . '$2"';
+		$text = preg_replace($pattern, $replace, $text);
+		
+		// Replace href="abc" with base
+		$pattern = 	'|<a(.*?) href="(?!\w+://)(.*?)"|';
+		$replace = '<a$1 href="' . $base . '$2"';
+		$text = preg_replace($pattern, $replace, $text);
+		
+		return $text;
+	}	
 } 
