ffa259f2468be4196a36b53742a165870c00ebf3
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-05-13 21:14:44 +0000

    Cacheble happens at Cache Facade now

--- trunk/gyro/core/model/classes/cache.db.impl.php
+++ trunk/gyro/core/model/classes/cache.db.impl.php
@@ -85,49 +85,15 @@ class CacheDBImpl implements ICachePersister {
 	/**
 	 * Clear the cache
 	 * 
-	 * @param Mixed A set of key params, may be an array or a string, or an ICachable instance. If NULL, all is cleared
+	 * @param Mixed A set of key params, may be an array or a string. If NULL, all is cleared
 	 */
 	public function clear($cache_keys = NULL) {
-		if (empty($cache_keys)) {
-			$this->do_clear_all();
-		}
-		else if ($cache_keys instanceof ICachable) {
-			$this->do_clear_cachable($cache_keys);
-			foreach($cache_keys->get_dependend_cachables() as $dependance) {
-				$this->do_clear_cachable($dependance);
-			}
-		}
-		else {
-			$this->do_clear($cache_keys);
-		}
-	}
-	
-	/**
-	 * Clear chache for given ICachable 
-	 */
-	private function do_clear_cachable($cachable) {
-		$keys = $cachable->get_all_cache_ids();
-		foreach($keys as $key) {
-			$this->do_clear($key);
-		}		
-	}
-	
-	/**
-	 * Clear all cache
-	 */
-	private function do_clear_all() {
 		$dao = new DAOCache();
-		$dao->delete(DAOCache::WHERE_ONLY);	
-	}
-	
-	/**
-	 * Clear cache for given cache key(s)
-	 */
-	private function do_clear($cache_keys) {
-		$keys = $this->extract_keys($cache_keys);
-		$dao = new DAOCache();
-		$dao->set_keys($keys, true);
-		$dao->delete(DAOCache::WHERE_ONLY);	
+		if (!empty($cache_keys)) {
+			$keys = $this->extract_keys($cache_keys);
+			$dao->set_keys($keys, true);
+		}
+		$dao->delete(DAOCache::WHERE_ONLY);				
 	}
 	
 	/**
--- trunk/gyro/core/model/classes/cache.facade.php
+++ trunk/gyro/core/model/classes/cache.facade.php
@@ -39,11 +39,12 @@ class Cache {
 	 * Returns true, if item is cached
 	 *  
 	 * @param $cache_keys mixed A set of key params, may be an array or a string
+	 * @param $ignore_disabled Lookup item, even if cache is disabled
 	 * @return bool 
 	 */
-	public static function is_cached($cache_keys) {
+	public static function is_cached($cache_keys, $ignore_disabled = false) {
 		// Allow diableing of cache
-		if (Config::has_feature(Config::DISABLE_CACHE)) {
+		if (!$ignore_disabled && Config::has_feature(Config::DISABLE_CACHE)) {
 			return false;			
 		}
 		
@@ -55,11 +56,12 @@ class Cache {
 	 * Read from cache
 	 * 
 	 * @param Mixed A set of key params, may be an array or a string
+	 * @param $ignore_disabled Lookup item, even if cache is disabled
 	 * @return ICacheItem False if cache is not found
 	 */
-	public static function read($cache_keys) {
+	public static function read($cache_keys, $ignore_disabled = false) {
 		// Allow diableing of cache
-		if (Config::has_feature(Config::DISABLE_CACHE)) {
+		if (!$ignore_disabled && Config::has_feature(Config::DISABLE_CACHE)) {
 			return false;			
 		}
 
@@ -74,11 +76,12 @@ class Cache {
 	 * @param string $content The cache
 	 * @param int $cache_life_time Cache life time in seconds
 	 * @param mixed $data Any data assoziated with this item
-	 * @param bool $is_compressed True, if $content is already gzip compressed 
+	 * @param bool $is_compressed True, if $content is already gzip compressed
+	 * @param $ignore_disabled Store item, even if cache is disabled 
 	 */
-	public static function store($cache_keys, $content, $cache_life_time, $data = '', $is_compressed = false) {
+	public static function store($cache_keys, $content, $cache_life_time, $data = '', $is_compressed = false, $ignore_disabled = false) {
 		// Allow diableing of cache
-		if (Config::has_feature(Config::DISABLE_CACHE)) {
+		if (!$ignore_disabled && Config::has_feature(Config::DISABLE_CACHE)) {
 			return;			
 		}
 		
@@ -96,13 +99,32 @@ class Cache {
 	 * Clear the cache
 	 * 
 	 * @param Mixed A set of key params, may be an array or a string, or an ICachable instance. If NULL, all is cleared
+	 * @param $ignore_disabled Clear item, even if cache is disabled
 	 */
-	public static function clear($cache_keys = NULL) {
+	public static function clear($cache_keys = NULL, $ignore_disabled = false) {
 		// Allow diableing of cache
-		if (Config::has_feature(Config::DISABLE_CACHE)) {
+		if (!$ignore_disabled && Config::has_feature(Config::DISABLE_CACHE)) {
 			return;			
 		}
 		
+		if ($cache_keys instanceof ICachable) {
+			$keys = $cache_keys->get_all_cache_ids();
+			foreach($keys as $key) {
+				self::do_clear($key);
+			}
+			foreach($cache_keys->get_dependend_cachables() as $dependance) {
+				self::clear($dependance);
+			}
+		}
+		else {
+			self::do_clear($cache_keys);
+		}	
+	}
+
+	/**
+	 * Invoke implemantation to clear a given item
+	 */
+	private static function do_clear($cache_keys) {
 		$impl = self::get_implementation();
 		$impl->clear($cache_keys);
 	}
