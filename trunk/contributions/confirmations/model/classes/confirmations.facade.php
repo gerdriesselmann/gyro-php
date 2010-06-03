d15740896e4099f4c262c76a243d9d0dd3898503
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-06-03 15:33:08 +0000

    Remove expired

--- trunk/contributions/confirmations/model/classes/confirmations.facade.php
+++ trunk/contributions/confirmations/model/classes/confirmations.facade.php
@@ -12,6 +12,8 @@ class Confirmations {
 	 * @return Status 
 	 */
 	public static function create($id_item, $data, $action, &$result = null) {
+		self::remove_expired();
+		
 		$params = array(
 			'id_item' => $id_item,
 			'data' => $data,
@@ -29,6 +31,8 @@ class Confirmations {
 	 * Create a confirmatin handler suitable for the desired action of confirmation for given code
 	 */
 	public static function create_confirmation_handler($id, $code) {
+		self::remove_expired();
+		
 		$confirmation = new DAOConfirmations();
 		$confirmation->code = $code;
 		$confirmation->id = $id;
@@ -37,5 +41,20 @@ class Confirmations {
 		}
 		// Default implementation handles missing confirmation...
 		return new ConfirmationHandlerBase(false);				
+	}
+	
+	/**
+	 * Remove expired confirmations
+	 * 
+	 * @return Status
+	 */
+	public static function remove_expired() {
+		$c = new DAOConfirmations();
+		// Don't delete all expired, since users won't get an "Expired" messages in that case, 
+		// only "Not found", which might be confusung.
+		// Deleting confirmations that expired a month ago (given that usual expiration time is 
+		// about a day) seems OK, though      
+		$c->add_where('expirationdate', '<', time() - 30 * GyroDate::ONE_DAY);
+		return $c->delete(DataObjectBase::WHERE_ONLY);
 	}
 }
