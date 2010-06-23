4cdc0f31c2d15c224a9d7f50d5e36d360c655658
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-06-23 11:55:15 +0000

    Added geo calculations

---
+++ contributions/lib.geocalc/doxygen.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @defgroup GeoCalc
+ * @ingroup Libs
+ * 
+ * Helper for geografic calculations 
+ * 
+ * @section Usage Usage
+ * 
+ * Enable as "lib.geocalc" 
+ * 
+ * The class itself is a component:
+ * 
+ * @code
+ * Load::components('geocalculator');
+ * $distance = GeoCalculator::distance($lat1, $lon1, $lat2, $lon2);
+ * @endcode
+ */
