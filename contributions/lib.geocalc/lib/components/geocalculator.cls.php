41bc5ea4a32863be950d93f1ee496568265d9720
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-07-15 11:35:05 +0000

    Enhancements on coordinate, added bounding box function

--- contributions/lib.geocalc/lib/components/geocalculator.cls.php
+++ contributions/lib.geocalc/lib/components/geocalculator.cls.php
@@ -6,15 +6,7 @@
  * @author Gerd Riesselmann
  */
 class GeoCalculator {
-	/**
-	 * Convert degrees into radion
-	 * 
-	 * @param float $degree A degree value (latitude or longitude)
-	 * @return float Radian value
-	 */
-	public static function to_radian($degree) {
-		return $degree * pi() /180;
-	}
+	const R = 6371; // Earth radius in km
 	
 	/**
 	 * Calculates distance between to points
@@ -27,12 +19,11 @@ class GeoCalculator {
 	 */
 	public static function distance($lat1, $lon1, $lat2, $lon2) {
 		// http://www.movable-type.co.uk/scripts/latlong.html
-		$lat1 = self::to_radian($lat1);
-		$lat2 = self::to_radian($lat2);
-		$lon1 = self::to_radian($lon1);
-		$lon2 = self::to_radian($lon2);
+		$lat1 = deg2rad($lat1);
+		$lat2 = deg2rad($lat2);
+		$lon1 = deg2rad($lon1);
+		$lon2 = deg2rad($lon2);
 		
-		$R = 6371; // km
 		$dLat = $lat2 - $lat1;
 		$dLon = $lon2 - $lon1; 
 		$a = 
@@ -40,7 +31,29 @@ class GeoCalculator {
 			cos($lat1) * cos($lat2) * 
 			sin($dLon/2) * sin($dLon/2); 
 		$c = 2 * atan2(sqrt($a), sqrt(1-$a)); 
-		$d = $R * $c;
+		$d = self::R * $c;
 		return $d;
+	}
+	
+	/**
+	 * Compute bounding box
+	 * 
+	 * @param float $lat Latitude
+	 * @param float $lon Longitude
+	 * @param float $ns_radius Radius of bound box in north to south direction
+	 * @param float $we_radius Radius of bound box in west to east direction
+	 * 
+	 * @return array Array with two elements 'lat' and 'lon', each containing an array 
+	 *               with two elements 'min' and 'max 
+	 */
+	public static function bounding_box($lat, $lon, $ns_radius, $we_radius) {
+		// http://www.movable-type.co.uk/scripts/latlong-db.html
+		$d_lat = rad2deg($ns_radius/self::R);
+		$d_lon = rad2deg($we_radius/self::R/cos(deg2rad($lat)));
+		
+		return array(
+			'lat' => array('min' => $lat - $d_lat, 'max' => $lat + $d_lat),
+			'lon' => array('min' => $lon - $d_lon, 'max' => $lon + $d_lon)
+		);
 	}
 }
