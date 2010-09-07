2b8a79342bcfe5b5be5409f05262ad4874631889
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-09-07 21:39:25 +0000

    Tests for a bugfix long ago, forgot to commit

--- trunk/gyro/modules/simpletest/simpletests/dbquerydelete.test.php
+++ trunk/gyro/modules/simpletest/simpletests/dbquerydelete.test.php
@@ -5,46 +5,46 @@ class DBQueryDeleteTest extends GyroUnitTestCase {
 		
 		$query = new DBQueryDelete($table);
 		$this->assertEqual(
-			"TRUNCATE table",
+			"TRUNCATE `table`",
 			$query->get_sql()
 		);
 
 		$query = new DBQueryDelete($table);
 		$query->set_limit(5);
 		$this->assertEqual(
-			"DELETE FROM table LIMIT 0,5",
+			"DELETE FROM `table` LIMIT 0,5",
 			$query->get_sql()
 		);
 		
 		$query = new DBQueryDelete($table);
 		$query->add_order('col1');
 		$this->assertEqual(
-			"DELETE FROM table ORDER BY col1 ASC",
+			"DELETE FROM `table` ORDER BY col1 ASC",
 			$query->get_sql()
 		);
 		
 		$query = new DBQueryDelete($table);
 		$query->add_where('column', '=', 1);
 		$this->assertEqual(
-			"DELETE FROM table WHERE ((`alias`.`column` = '1'))",
+			"DELETE FROM `table` WHERE ((`alias`.`column` = '1'))",
 			$query->get_sql()
 		);
 		
 		$query->set_limit(5);
 		$this->assertEqual(
-			"DELETE FROM table WHERE ((`alias`.`column` = '1')) LIMIT 0,5",
+			"DELETE FROM `table` WHERE ((`alias`.`column` = '1')) LIMIT 0,5",
 			$query->get_sql()
 		);
 
 		$query->add_order('col1');
 		$this->assertEqual(
-			"DELETE FROM table WHERE ((`alias`.`column` = '1')) ORDER BY col1 ASC LIMIT 0,5",
+			"DELETE FROM `table` WHERE ((`alias`.`column` = '1')) ORDER BY col1 ASC LIMIT 0,5",
 			$query->get_sql()
 		);
 		
 		$query->add_order('col2', DBQueryDelete::DESC);
 		$this->assertEqual(
-			"DELETE FROM table WHERE ((`alias`.`column` = '1')) ORDER BY col1 ASC, col2 DESC LIMIT 0,5",
+			"DELETE FROM `table` WHERE ((`alias`.`column` = '1')) ORDER BY col1 ASC, col2 DESC LIMIT 0,5",
 			$query->get_sql()
 		);
 	}
