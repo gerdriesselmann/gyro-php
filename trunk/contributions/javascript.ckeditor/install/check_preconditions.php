c7dbcddda1f6512b8ba797e904186d6447d09e68
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-09-28 11:59:02 +0000

    Upgrade to CKEditor 3.4.1

--- trunk/contributions/javascript.ckeditor/install/check_preconditions.php
+++ trunk/contributions/javascript.ckeditor/install/check_preconditions.php
@@ -2,7 +2,7 @@
 function javascript_ckeditor_check_preconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('javascript.ckeditor') . 'data/3.3/';
+	$root = Load::get_module_dir('javascript.ckeditor') . 'data/3.4/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js'), SystemUpdateInstaller::COPY_OVERWRITE));
 	return $ret;
 }
