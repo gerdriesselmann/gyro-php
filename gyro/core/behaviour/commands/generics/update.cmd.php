df8ec6d6b85952ee9a35e85565f64d7828f62612
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-10-04 11:25:21 +0000

    Unset all primary keys before updating

--- gyro/core/behaviour/commands/generics/update.cmd.php
+++ gyro/core/behaviour/commands/generics/update.cmd.php
@@ -14,7 +14,14 @@ class UpdateCommand extends CommandTransactional  {
 	protected function do_execute() {
 		$ret = new Status();
 	
+		/* @var $inst DataObjectBase */
 		$inst = $this->get_instance();
+		$params = $this->get_params();
+		// Unset primary keys on params, so e.g. ID cannot be changed by an update
+		foreach($inst->get_table_keys() as $col => $field) {
+			unset($params[$col]);
+		}
+		
 		$inst->read_from_array($this->get_params());
 		$ret->merge($inst->validate());
 		if ($ret->is_ok()) {
