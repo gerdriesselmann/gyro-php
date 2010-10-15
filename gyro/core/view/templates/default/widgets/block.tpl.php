a95b0b96d0f01f34e7915ea24cdc1619ade3af69
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-10-15 18:40:08 +0000

    Intriducing interface for blocks and make blocks self-renderable

--- gyro/core/view/templates/default/widgets/block.tpl.php
+++ gyro/core/view/templates/default/widgets/block.tpl.php
@@ -4,11 +4,11 @@
  * @var BlockBase $block The block to display
  */
 $block_html = '';
-if ($block->title !== '') {
-	$block_html .= html::h(String::escape($block->title), 2);
+if ($block->get_title() !== '') {
+	$block_html .= html::h(String::escape($block->get_title()), 2);
 }
-$block_html .= html::div($block->content);
-$cls = 'block block-' . String::plain_ascii($block->name); 
+$block_html .= html::div($block->get_content());
+$cls = 'block block-' . String::plain_ascii($block->get_name()); 
 $block_html = html::div($block_html, $cls);
 
 print $block_html;
--- gyro/core/view/templates/default/widgets/blocks.tpl.php
+++ gyro/core/view/templates/default/widgets/blocks.tpl.php
@@ -1,6 +1,4 @@
 <?php
 foreach ($blocks as $block) {
-	$view = ViewFactory::create_view(IViewFactory::MESSAGE, 'widgets/block');
-	$view->assign('block', $block);
-	print $view->render();
+	print $block->render();
 }
