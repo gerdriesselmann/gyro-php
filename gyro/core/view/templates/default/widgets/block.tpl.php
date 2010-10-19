c5695c28e7850a938a8e174b4caa6b3ea66c947e
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-10-19 19:18:05 +0000

    Add special content class

--- gyro/core/view/templates/default/widgets/block.tpl.php
+++ gyro/core/view/templates/default/widgets/block.tpl.php
@@ -7,7 +7,7 @@ $block_html = '';
 if ($block->get_title() !== '') {
 	$block_html .= html::h(String::escape($block->get_title()), 2);
 }
-$block_html .= html::div($block->get_content());
+$block_html .= html::div($block->get_content(), 'block-content');
 $cls = 'block block-' . String::plain_ascii($block->get_name()); 
 $block_html = html::div($block_html, $cls);
 
