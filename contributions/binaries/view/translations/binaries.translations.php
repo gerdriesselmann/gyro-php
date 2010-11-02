f8a10f7cd957ab0d923c1237559d0a3b2c577ae9
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-11-02 12:10:16 +0000

    Added translations

---
+++ contributions/binaries/view/translations/binaries.translations.php
@@ -0,0 +1,14 @@
+<?php
+function binaries_load_translations($languages) {
+	return array(
+		'No file was uploaded' => array(
+			'de' => 'Es wurde keine Datei hochgeladen'
+		),
+		'The uploaded file is too big' => array(
+			'de' => 'Die hochgeladene Datei ist zu groß',
+		),
+		'An unknown error code was retrieved while uploading the file.' => array(
+			'de' => 'Beim Hochladen der Datei ist ein Fehler aufgetreten.'
+		)
+	);
+}
