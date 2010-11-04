a6afdb9d57e7bbc346a848918d6d8cea0dd267e5
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-11-04 18:20:20 +0000

    Typo

--- contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
+++ contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
@@ -5,8 +5,8 @@ willkommen bei <?php print $appname; ?>. Sie haben sich mit folgenden Daten regi
 Benutzername: <?php print $user->name ?> 
 E-Mail: <?php print $user->email; ?>
 
-Fallse Sie sich nicht auf <?php print $appname; ?> registriert haben, ignorieren Sie 
-bitte diese Nachricht oder kontaktieren Sie uns um den Missbrauch unserer Dienste zu melden. 
+Falls Sie sich nicht auf <?php print $appname; ?> registriert haben, ignorieren Sie 
+bitte diese Nachricht oder kontaktieren Sie uns, um den Missbrauch unserer Dienste zu melden. 
 Ansonsten besuchen Sie 
 
 <?php print ActionMapper::get_url('confirm', $confirmation); ?>
