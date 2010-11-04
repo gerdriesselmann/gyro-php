bab658e011f831b64945517d3dd63c6b94d8c929
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-11-04 18:08:36 +0000

    Different cache header policy implemententations to be switched

---
+++ trunk/contributions/page.cacheheaders/view/base/cacheheaderspageview.cls.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * An overloaded PageView to serve other cache headers
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup CacheHeaders
+ */
+class CacheHeadersPageView extends PageViewBase {
+	/**
+	 * Send cache control headers for cache
+	 *
+	 * @param $lastmodified A timestamp 
+	 * @param $expires A timestamp
+	 * @param $max_age Max age in seconds
+	 */
+	protected function send_cache_headers($lastmodified, $expires, $max_age = 600, $etag = '') {
+		$max_age = intval($max_age);
+		Common::header('Pragma', '', false);
+		Common::header('Last-Modified', GyroDate::http_date($lastmodified), false);
+		Common::header('Expires', GyroDate::http_date($expires), false);
+		Common::header('Etag', $etag, true);
+		switch (Config::get_value(ConfigCacheHeaders::CACHE_POLICY)) {
+			case ConfigCacheHeaders::RIGID_FRESHNESS:
+			default:
+				Common::header('Cache-Control', "private, must-revalidate, no-cache", false);		
+		}		
+	}
+}
