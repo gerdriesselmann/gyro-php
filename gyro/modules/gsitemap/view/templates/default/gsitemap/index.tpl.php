eb4221a8df70b181820c45ba2df8a476bbf63fcc
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2010-11-16 20:31:58 +0000

    Remove unnecessary timestamp from index file

--- gyro/modules/gsitemap/view/templates/default/gsitemap/index.tpl.php
+++ gyro/modules/gsitemap/view/templates/default/gsitemap/index.tpl.php
@@ -1,8 +1,7 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd">
 <?php foreach ($files as $item): ?>
 <sitemap>
-	<loc><?=$baseurl_http?>sitemap.xml?i=<?=$item?></loc>
-	<?php if ($timestamp): ?><lastmod><?php print GyroDate::iso_date($timestamp); ?></lastmod><?php endif; ?>
+	<loc><?=$baseurl_http?>sitemap.xml?i=<?=$item?></loc>	
 </sitemap>
 <?php endforeach; ?>
 </sitemapindex>
