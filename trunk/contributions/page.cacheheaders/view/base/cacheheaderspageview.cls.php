72de4da98e2204e3a83083c0616c995729de2da7
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2010-12-01 19:11:28 +0000

    Cache headers should now work in IE too

--- trunk/contributions/page.cacheheaders/view/base/cacheheaderspageview.cls.php
+++ trunk/contributions/page.cacheheaders/view/base/cacheheaderspageview.cls.php
@@ -15,14 +15,14 @@ class CacheHeadersPageView extends PageViewBase {
 	 */
 	protected function send_cache_headers($lastmodified, $expires, $max_age = 600, $etag = '') {
 		$max_age = intval($max_age);
-		Common::header('Pragma', '', false);
-		Common::header('Last-Modified', GyroDate::http_date($lastmodified), false);
-		Common::header('Expires', GyroDate::http_date($expires), false);
-		Common::header('Etag', $etag, true);
+		GyroHeaders::remove('Pragma');
+		GyroHeaders::set('Last-Modified', GyroDate::http_date($lastmodified), true);
+		GyroHeaders::set('Expires', GyroDate::http_date($expires), true);
+		GyroHeaders::set('Etag', $etag, true);
 		switch (Config::get_value(ConfigCacheHeaders::CACHE_POLICY)) {
 			case ConfigCacheHeaders::RIGID_FRESHNESS:
 			default:
-				Common::header('Cache-Control', "private, must-revalidate, no-cache, max-age=0, pre-check=0", false);		
+				GyroHeaders::set('Cache-Control', "private, must-revalidate,max-age=0", true);		
 		}		
 	}
 }
