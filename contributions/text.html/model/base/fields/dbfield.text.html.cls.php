94a9d6d715e6aeab4546b7e56d9a37832db1a876
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-01-20 20:05:51 +0000

    If a field does contain nothing but HTML, regard it as beeing empty

--- contributions/text.html/model/base/fields/dbfield.text.html.cls.php
+++ contributions/text.html/model/base/fields/dbfield.text.html.cls.php
@@ -13,6 +13,37 @@ class DBFieldTextHtml extends DBFieldText {
 	}
 	
 	/**
+	 * Returns true, if the value passed fits the fields restrictions
+	 *
+	 * @param string $value
+	 * @return Status
+	 */
+	public function validate($value) {
+		$ret = new Status();
+		if (!$this->is_null($value)) {
+			$value = $this->convert_value($value);
+		}
+		if ($value !== '') {
+			// This test for empty HTML, like <p><span></span></p>, which will not be allowed
+			// @TODO what about <img />?
+	 		$test = String::preg_replace('|\W|ms', '', strip_tags($value)); 
+	 		if ($test === '' && !$this->get_null_allowed()) {
+				$ret->append(tr(
+					'%field may not be empty', 
+					'core', 
+					array(
+						'%field' => $this->get_field_name_translation(),
+					)
+				));
+			}
+		}
+		if($ret->is_ok()) {
+			$ret->merge(parent::validate($value));
+		}
+		return $ret;
+	}
+	
+	/**
 	 * Format values that are not NULL
 	 * 
 	 * @param mixed $value
@@ -20,7 +51,14 @@ class DBFieldTextHtml extends DBFieldText {
 	 */
 	protected function do_format_not_null($value) {
 		return parent::do_format_not_null(
-			HtmlText::apply_conversion(HtmlText::STORAGE, $value, $this->get_table()->get_table_name())
+			$this->convert_value($value)
 		);
+	}
+	
+	/**
+	 * Apply storage conversion
+	 */
+	protected function convert_value($value) {
+		return HtmlText::apply_conversion(HtmlText::STORAGE, $value, $this->get_table()->get_table_name());
 	}
 }
