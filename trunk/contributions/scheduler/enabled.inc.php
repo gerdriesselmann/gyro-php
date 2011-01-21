a5926a39d34df717ac953827ee473f7a4a9809ca
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-01-21 15:01:16 +0000

    Make sending mails on error an optional choice

--- trunk/contributions/scheduler/enabled.inc.php
+++ trunk/contributions/scheduler/enabled.inc.php
@@ -14,3 +14,17 @@ Load::enable_module('console');
  * Tasks can configured to be repeated a given number of times, even with different frequence.
  * Reschedule policies can be different in case of success or failure of a task.
  */
+
+/**
+ * Defines Scheduler Config Options
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup Scheduler
+ *
+ */
+class ConfigScheduler {
+	/** Whether to send mails on errors or not */
+	const SEND_ERROR_MAIL = 'SCHEDULER_SEND_ERROR_MAIL';
+}
+
+Config::set_feature_from_constant(ConfigScheduler::SEND_ERROR_MAIL, 'APP_SCHEDULER_SEND_ERROR_MAIL', true);
