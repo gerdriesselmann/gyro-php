e2ae4baad582fac4c4ba84276c7cbf2e9a47ac9b
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-02-08 21:16:57 +0000

    Pass model, if set

--- trunk/contributions/models.postbase/view/templates/default/postbase/widgets/content.tpl.php
+++ trunk/contributions/models.postbase/view/templates/default/postbase/widgets/content.tpl.php
@@ -3,6 +3,6 @@
 <?php
 print WidgetInput::output('title', tr('Title:', 'postbase'), $form_data);
 print WidgetInput::output('teaser', tr('Teaser:', 'postbase'), $form_data, WidgetInput::TEXTAREA);
-print WidgetInput::output('text', tr('Description:', 'postbase'), $form_data, HtmlText::WIDGET);
+print WidgetInput::output('text', tr('Description:', 'postbase'), $form_data, HtmlText::WIDGET, array('model' => $model));
 ?>
 </fieldset>
