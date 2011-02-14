140289f00dfb1de945e481914decab3f6c9f326d
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-02-14 19:46:43 +0000

    No need to check for PEAR Mail and Mail_Mime any more.

--- trunk/gyro/install/check_preconditions.php
+++ trunk/gyro/install/check_preconditions.php
@@ -1,10 +1,3 @@
 <?php
 function core_check_preconditions() {
-	$pear_classes = array(
-		'Mail',
-		'Mail_mime'
-	);
-	Load::components('installedvalidator');
-	$v = new InstalledValidator();
-	return $v->validate_pear_modules_are_installed($pear_classes);
 }
