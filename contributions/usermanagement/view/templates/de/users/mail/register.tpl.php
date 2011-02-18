137a2753d514377b63b25d7bf3271f5c8c293eab
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-02-18 14:57:54 +0000

    Turn plain text mails into HTML with alternate plain text

--- contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
+++ contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
@@ -1,18 +1,28 @@
-Hallo,
+<?php 
+$mailcmd->set_is_html(true);
+$mailcmd->set_alt_message($self);
+?>
+<p><b>Hallo,</b></p>
 
-willkommen bei <?php print $appname; ?>. Sie haben sich mit folgenden Daten registriert:
-
-Benutzername: <?php print $user->name ?> 
-E-Mail: <?php print $user->email; ?>
-
-Falls Sie sich nicht auf <?php print $appname; ?> registriert haben, ignorieren Sie 
+<p>
+willkommen bei <?=$appname; ?>. Sie haben sich mit folgenden Daten registriert:
+</p>
+<p>
+Benutzername: <b><?=$user->name ?></b> <br />
+E-Mail: <b><?=$user->email; ?></b>
+</p>
+<p>
+Falls Sie sich nicht auf <?=$appname; ?> registriert haben, ignorieren Sie 
 bitte diese Nachricht oder kontaktieren Sie uns, um den Missbrauch unserer Dienste zu melden. 
 Ansonsten besuchen Sie 
-
-<?php print ActionMapper::get_url('confirm', $confirmation); ?>
-
+</p>
+<p>
+<a href="<?=ActionMapper::get_url('confirm', $confirmation); ?>"><?=ActionMapper::get_url('confirm', $confirmation); ?></a>
+</p>
+<p>
 um Ihre Registrierung abzuschließen.
-
-Mit freundlichen Grüßen,
-Das Team von <?php print $appname?>
-
+</p>
+<p>
+Mit freundlichen Grüßen,<br />
+Das Team von <?=$appname?>
+</p>
