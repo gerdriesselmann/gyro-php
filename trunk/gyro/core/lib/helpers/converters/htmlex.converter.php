a73e54aacf43cca37d6c7d38e5748ad85a933266
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-02-18 18:33:32 +0000

    Params for HTMLEX decoder to allow better situation depended decoding

--- trunk/gyro/core/lib/helpers/converters/htmlex.converter.php
+++ trunk/gyro/core/lib/helpers/converters/htmlex.converter.php
@@ -30,22 +30,54 @@ class ConverterHtmlEx extends ConverterHtml {
 	 * Smarter decoding og html into plain text than done by HTML Converter
 	 * 
 	 * Keeps Links
+	 * 
+	 * The following parameters are supported:
+	 * 
+	 * @param array $params 
+	 *   Associative array supporting the following featurs:
+	 *   @li p: Text after paragraph. Default is "\n"
+	 *   @li br: Text after a <br> tag. Default is "\n"
+	 *   @li a: Format to decode <a> tags. Supports $title$ and $url$ placeholders. Default is "$title$: $url$"
 	 */
 	public function decode($value, $params = false) {
-		// Process Links
-		$value = $this->relative_to_absolute($value, Config::get_url(Config::URL_BASEURL));
-		// Turn <a href="xxx">Text</a> into Text: xxx
-		$value = String::preg_replace('|<a.*?href="(.*?)".*?>(.*?)</a>|', '$2: $1', $value);
+		// If there is no HTML, decoding would do more harm than good
+		if (!preg_match('@<\w+.*?>@', $value)) {
+			return $value;
+		}
 		
-		$value = str_replace('</p>', "</p>\n\n", $value);
-		$value = preg_replace('@<br.*?>@', "\n", $value);
+		// Turn <a href="xxx">Text</a> into e.g. Text: xxx
+		$value = $this->decode_anchors($value, Arr::get_item($params, 'a', '$title$: $url$'));
+		
+		// Remove paragraphs
+		$value = str_replace("\r", "\n", $value);
+		$value = String::preg_replace('|\n+|', " ", $value);
+		
+		// Replace <p> and <br>
+		$value = preg_replace('@\s*<p>\s*@', "<p>", $value);
+		$after_p = Arr::get_item($params, 'p', "\n");
+		$value = preg_replace('@\s*</p>\s*@', "</p>$after_p", $value);
+		$after_br = Arr::get_item($params, 'br', "\n");
+		$value = preg_replace('@<br.*?>\s*@', $after_br, $value);
+		
+		// Strip HTML
 		$value = strip_tags($value);
 		$value = String::unescape($value);
 		$value = String::preg_replace('| +|', ' ', $value);
 		
-		$value = str_replace("\r", "\n", $value);
-		$value = String::preg_replace('|\n+|', "\n", $value);
 		return $value;		
+	}
+	
+	/**
+	 * Decode a HTML <a> tag into plain text
+	 * 
+	 * @param string $value
+	 * @param string $link_format Supports placeholders $title$ and $url$
+	 */
+	protected function decode_anchors($value, $link_format) {
+		$value = $this->relative_to_absolute($value, Config::get_url(Config::URL_BASEURL));
+		$link_format = str_replace('$title$', '$2', $link_format);
+		$link_format = str_replace('$url$', '$1', $link_format);
+		return String::preg_replace('|<a.*?href="(.*?)".*?>(.*?)</a>|', $link_format, $value);
 	}
 
 	/**
