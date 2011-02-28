01dd9252c52ecf2c1d98d4822606070a053824f3
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-02-28 20:03:17 +0000

    Validate and if required create temporary directories

--- trunk/gyro/install/check_preconditions.php
+++ trunk/gyro/install/check_preconditions.php
@@ -1,3 +1,29 @@
 <?php
 function core_check_preconditions() {
+	$ret = new Status();
+	
+	$tempdir = Config::get_value(Config::TEMP_DIR);
+	$subdirs = array(
+		'',
+		'log', 
+		'view', 'view/templates_c'
+	);
+	foreach($subdirs as $subdir) {
+		$dir = $tempdir . $subdir;
+		if (!file_exists($dir)) {
+			if (!mkdir($pathname, 0777, true)) {
+				$ret->append('Could not create temporary directory ' . $dir);			
+			}
+		}
+		// Try to place file into temp dir
+		$file = $dir . '/test' . md5(uniqid());
+		if (touch($file)) {
+			unlink($file);
+		}
+		else {
+			$ret->append('Could not create file in temporary directory ' . $dir);
+		}
+	}
+	
+	return $ret;
 }
