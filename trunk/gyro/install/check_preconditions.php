b751258faebdd366ee10cab2224c4511cacf661e
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-02-28 20:19:51 +0000

    Now it actually works...

--- trunk/gyro/install/check_preconditions.php
+++ trunk/gyro/install/check_preconditions.php
@@ -2,17 +2,21 @@
 function core_check_preconditions() {
 	$ret = new Status();
 	
-	$tempdir = Config::get_value(Config::TEMP_DIR);
+	$tempdir = realpath(Config::get_value(Config::TEMP_DIR));
 	$subdirs = array(
 		'',
 		'log', 
 		'view', 'view/templates_c'
 	);
 	foreach($subdirs as $subdir) {
-		$dir = $tempdir . $subdir;
+		$dir = rtrim($tempdir . $subdir, '/');
 		if (!file_exists($dir)) {
-			if (!mkdir($pathname, 0777, true)) {
+			$cmd = 'mkdir -p ' . $dir;
+			if (shell_exec($cmd)) { 
 				$ret->append('Could not create temporary directory ' . $dir);			
+			}
+			else {
+				chmod($dir, 0777);
 			}
 		}
 		// Try to place file into temp dir
