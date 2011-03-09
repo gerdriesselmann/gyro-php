dee61f2331c905e323eefcd4acfcba8420fa32c6
catarina.em.casa <catarina.em.casa@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-03-09 16:47:50 +0000

    Ticket #407

--- contributions/usermanagement/view/templates/de/users/mail/onetimelogin.tpl.php
+++ contributions/usermanagement/view/templates/de/users/mail/onetimelogin.tpl.php
@@ -2,7 +2,7 @@
 $mailcmd->set_is_html(true);
 $mailcmd->set_alt_message($self);
 ?>
-<p><b>Hallo,</b></p>
+<p><b>Hallo!</b></p>
 
 <p>Sie erhalten diese Mail damit Sie ein neues Passwort für Ihr Benutzerkonto vergeben können. 
 Durch den folgenden Link werden Sie einmalig angemeldet und können dadurch ein neues Passwort setzen:</p>
@@ -13,4 +13,4 @@ Durch den folgenden Link werden Sie einmalig angemeldet und können dadurch ein 
 
 <p>Mit freundlichen Grüßen,</p>
 
-<p><?=$appname?></p>
+<p><b><?=$appname?></b></p>
--- contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
+++ contributions/usermanagement/view/templates/de/users/mail/register.tpl.php
@@ -2,7 +2,7 @@
 $mailcmd->set_is_html(true);
 $mailcmd->set_alt_message($self);
 ?>
-<p><b>Hallo,</b></p>
+<p><b>Hallo!</b></p>
 
 <p>Sie erhalten diese Mail weil Sie sich gerade registriert haben. Durch den folgenden Link können Sie Ihre Registrierung bestätigen und die 
 Registrierung abschließen:</p>
@@ -11,11 +11,11 @@ Registrierung abschließen:</p>
 
 <p>Die Registrierung erfolgte mit folgenden Daten:</p>
 
-<p>Benutzername: <b><?=$user->name ?></b> <br />
-E-Mail: <b><?=$user->email; ?></b></p>
+<p>Benutzername: <?=$user->name ?><br />
+E-Mail: <?=$user->email; ?></p>
 
 <p>Falls Sie sich nicht registriert haben, so ignorieren Sie bitte diese Nachricht oder kontaktieren Sie uns, um den Missbrauch unserer Dienste zu melden.</p>
 
 <p>Mit freundlichen Grüßen,</p>
 
-<p><?=$appname?></p>
+<p><b><?=$appname?></b></p>
