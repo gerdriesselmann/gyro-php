6493055f99779c7c30d9a91f30cb779fb0ebde34
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-03-22 11:03:38 +0000

    Use new client cache possibilites to send different cache HTTP headers when logged in or not

--- trunk/contributions/usermanagement/controller/base/cachemanager/anonymouscachemanager.cls.php
+++ trunk/contributions/usermanagement/controller/base/cachemanager/anonymouscachemanager.cls.php
@@ -2,6 +2,9 @@
 /**
  * Cache manager that caches only if user is not logged in
  * 
+ * If user is logged in, it returns the CacheHeaderManager set
+ * as ConfigUsermanagement::CACHEHEADER_CLASS_LOGGEDIN
+ * 
  * Returns url as cache key
  * 
  * @author Gerd Riesselmann
@@ -17,4 +20,18 @@ class AnonymousCacheManager extends SuccessCacheManager {
 		}
 		return ''; 
 	}
+	
+	/**
+	 * Get cache header manager
+	 * 
+	 * @return ICacheHeaderManager
+	 */
+	public function get_cache_header_manager() {
+		if (Users::is_logged_in()) {
+			return CacheHeaderManagerFactory::create(Config::get_value(ConfigUsermanagement::CACHEHEADER_CLASS_LOGGEDIN));
+		}
+		else {
+			return parent::get_cache_header_manager();
+		}
+	}		
 }
