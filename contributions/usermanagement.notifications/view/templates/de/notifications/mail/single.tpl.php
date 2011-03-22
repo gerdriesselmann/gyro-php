97874490e5a29a3114ae8cc70c9af80ad87b178b
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-03-22 20:33:55 +0000

    Turn mails into HTML

--- contributions/usermanagement.notifications/view/templates/de/notifications/mail/single.tpl.php
+++ contributions/usermanagement.notifications/view/templates/de/notifications/mail/single.tpl.php
@@ -1,13 +1,27 @@
-Hallo,
+<?php
+$mailcmd->set_is_html(true);
+$mailcmd->set_alt_message($self);
+$link_settings = ActionMapper::get_url('notifications_settings')
+?>
+<p><b>Hallo!</b></p>
 
-bei <?php print $appname ?> ist etwas geschehen.
+<p>
+bei <?=$appname?> ist etwas geschehen.
+</p>
 
--- <?php print $notification->get_title();?> --
-<?php print wordwrap(ConverterFactory::decode($notification->get_message(Notifications::DELIVER_MAIL), ConverterFactory::HTML_EX), 65); ?> 
+<p><b><?=$notification->get_title();?></b></p>
 
-Um Ihre Einstellungen für Benachrichtigungen zu ändern loggen Sie sich bei <?php print $appname ?> ein und besuchen Sie
-<?php print ActionMapper::get_url('notifications_settings')?>.
+<?php print $notification->get_message(Notifications::DELIVER_MAIL); ?> 
 
-Mit freundlichen Grüßen,
-Das Team von <?php print $appname?>
+<p>
+Um Ihre Einstellungen für Benachrichtigungen zu ändern loggen Sie sich bei <?=$appname?> ein und besuchen Sie
+</p>
 
+
+<p>
+<a href="<?=$link_settings?>"><?=$link_settings?></a>
+</p>
+
+<p><br />Mit freundlichen Grüßen,</p>
+
+<p><b><?=$appname?></b></p>
