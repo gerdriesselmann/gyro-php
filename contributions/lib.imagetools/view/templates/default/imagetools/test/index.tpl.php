5661be6c52b36adf160c99b7997ab81e1046f78a
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-03-30 18:48:56 +0000

    Add fit algorithm

--- contributions/lib.imagetools/view/templates/default/imagetools/test/index.tpl.php
+++ contributions/lib.imagetools/view/templates/default/imagetools/test/index.tpl.php
@@ -19,6 +19,9 @@ Upload an image to see imagetool's results: <input name="upload" type="file" /><
 <p>This is file resized to 300x100: </p>
 <div><img src="<?=ActionMapper::get_path('imagetools_test_image', array('type' => 'resize'))?>" alt="Resized to 300x100" /></div>
 
+<p>This is file fitted to 300x100: </p>
+<div><img src="<?=ActionMapper::get_path('imagetools_test_image', array('type' => 'fit'))?>" alt="Fitted to 300x100" /></div>
+
 <p>This is file croped in the middle 100x100: </p>
 <div><img src="<?=ActionMapper::get_path('imagetools_test_image', array('type' => 'crop'))?>" alt="Cropped 100x100" /></div>
 
