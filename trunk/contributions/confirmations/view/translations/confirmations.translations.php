f03c4e606c02c27333644471e46c5caabeaaf016
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-04-08 18:48:08 +0000

    Adding translations for German

---
+++ trunk/contributions/confirmations/view/translations/confirmations.translations.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Confirmation translations
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup Confirmations
+ */
+function confirmations_load_translations($languages) {
+	return array(
+		'No confirmations found for your request' => array(
+			'de' => 'Es wurde keine passenden Einträge für Ihren Code gefunden.'
+		),
+		'Your request has already expired and could not be confirmed' => array(
+			'de' => 'Ihre Anfrage kommt leider zu spät, die Zeit ist bereits abgelaufen.'
+		),
+		'Your request was confirmed' => array(
+			'de' => 'Ihre Anfrage wurde bestätigt'
+		),
+		'Confirmation' => array(
+			'de' => 'Bestätigung'
+		)
+	);
+}
+?>
