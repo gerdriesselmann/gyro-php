e518c6ab9ab7403ec0a7d8ccfd506d209b6bd29a
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-04-20 19:34:06 +0000

    No clean for partial documents

--- trunk/gyro/modules/tidy/lib/helpers/converters/htmltidy.converter.php
+++ trunk/gyro/modules/tidy/lib/helpers/converters/htmltidy.converter.php
@@ -28,27 +28,28 @@ class ConverterHtmlTidy implements IConverter {
 		if (String::starts_with(trim(String::to_lower($value)), '<script')) {
 			return $value;
 		}
+		$is_partial_doc = (strpos($value, '<html') === false);
 		$predefined_params = array(
-            'bare' => true,
-            'clean' => true,		
-            'drop-empty-paras' => true,
-            'drop-font-tags' => true,
-            'drop-proprietary-attributes' => true,
-            'enclose-block-text' => true,
-  			'enclose-text' => true,
-  			'indent' => true,
-            'join-classes' => false,
-            'join-styles' => false,
-            'logical-emphasis' => true,
-            'output-xhtml' => true,
+			'bare' => true,
+			'clean' => !$is_partial_doc,		
+			'drop-empty-paras' => true,
+			'drop-font-tags' => true,
+			'drop-proprietary-attributes' => true,
+			'enclose-block-text' => true,
+			'enclose-text' => true,
+			'indent' => true,
+			'join-classes' => false,
+			'join-styles' => false,
+			'logical-emphasis' => true,
+			'output-xhtml' => true,
 			'doctype' => 'loose',
-            'show-body-only' => (strpos($value, '<html') === false),
+			'show-body-only' => $is_partial_doc, 
 			'merge-divs' => false,
 			//'merge-spans' => false, // Not widely supported on Debian system
 			'hide-comments' => true,
 			'lower-literals' => true,
 			'char-encoding' => String::plain_ascii(GyroLocale::get_charset(), ''),
-            'wrap' => 0
+			'wrap' => 0
 		);
 		$params = array_merge($predefined_params, Arr::force($params, false));
 		$tidy = tidy_parse_string($value, $params, String::plain_ascii(GyroLocale::get_charset(), ''));
