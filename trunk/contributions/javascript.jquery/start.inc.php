a46458d300eb4588f237f34f8b4a52096e968725
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-05-06 22:01:55 +0000

    Now supports loading jQuery from CDN

--- trunk/contributions/javascript.jquery/start.inc.php
+++ trunk/contributions/javascript.jquery/start.inc.php
@@ -39,9 +39,32 @@ EventSource::Instance()->register(new JavascriptJQueryEventSink());
  * @ingroup JQuery
  */
 class ConfigJQuery {
+	/** @deprecated Use VERSION instead */
 	const JQUERY_VERSION = 'JQUERY_VERSION';
+	const VERSION = 'JQUERY_VERSION';
+	
+	/** @deprecated Use ON_EVERY_PAGE instead */	
 	const JQUERY_ON_EVERY_PAGE = 'JQUERY_ON_EVERY_PAGE';
+	const ON_EVERY_PAGE = 'JQUERY_ON_EVERY_PAGE';
+	
+	/**
+	 * JQuery CDN URL. Either an URL with %version% as placeholder for version OR "google" for the Google CDN
+	 * 
+	 * @attention %version% will be allways 3 digits (e.g. 1.6.0). Use %version_min% to force this to 1.6
+	 * 
+	 * Currently, only Google CDN is supported out of the box, since it's the only once with SSL support
+	 */
+	const CDN = 'JQUERY_CDN';
 }
 
-Config::set_value_from_constant(ConfigJQuery::JQUERY_VERSION, 'APP_JQUERY_VERSION', '1.4');
-Config::set_feature_from_constant(ConfigJQuery::JQUERY_ON_EVERY_PAGE, 'APP_JQUERY_ON_EVERY_PAGE', true);
+Config::set_feature_from_constant(ConfigJQuery::ON_EVERY_PAGE, 'APP_JQUERY_ON_EVERY_PAGE', true);
+Config::set_value_from_constant(ConfigJQuery::CDN, 'APP_JQUERY_CDN', '');
+
+// To be changed on new releases
+Config::set_value_from_constant(ConfigJQuery::VERSION, 'APP_JQUERY_VERSION', '1.5');
+define('JQUERY_VERSION_1_6', '1.6.0');
+define('JQUERY_VERSION_1_5', '1.5.2');
+
+define('JQUERY_VERSION_1_4', '1.4.4');
+define('JQUERY_VERSION_1_3', '1.3.2');
+
