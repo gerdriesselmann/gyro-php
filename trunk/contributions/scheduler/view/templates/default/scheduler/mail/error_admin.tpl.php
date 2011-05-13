daa8804fb5fcb7fe5ef5a85ebaf26922034b5563
catarina.em.casa <catarina.em.casa@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2011-05-13 08:34:27 +0000

    0000408: E-Mail ändern im Usermodul umbauen, übersetzungen 0000406: Alle Mails auf Alternate (HTML + Text) umstellen -> Scheduler mail umgewandelt

--- trunk/contributions/scheduler/view/templates/default/scheduler/mail/error_admin.tpl.php
+++ trunk/contributions/scheduler/view/templates/default/scheduler/mail/error_admin.tpl.php
@@ -1,5 +1,8 @@
 <?php
-print $task->name . ' (' . $task->action . ') - ' . GyroDate::local_date(time());
-print "\n\n";
-print $error->render(Status::OUTPUT_PLAIN);
+$mailcmd->set_is_html(true);
+$mailcmd->set_alt_message($self);
+?>
+<p><?=$task->name . ' (' . $task->action . ') - ' . GyroDate::local_date(time())?></p>
+<br /><br />
+<p><?=$error->render(Status::OUTPUT_PLAIN)?></p>
 
