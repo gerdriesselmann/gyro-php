0081260ed8830c9d3f275d9481d5200cfe4270b5
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-09-29 17:56:35 +0000

    Fix bug where only on of items with same name was shown

--- gyro/core/view/templates/default/widgets/menu/items.tpl.php
+++ gyro/core/view/templates/default/widgets/menu/items.tpl.php
@@ -1,27 +1,32 @@
 <?php
+/**
+ * @var $action IActionBase
+ */
 $action_view = ViewFactory::create_view(IViewFactory::MESSAGE, 'widgets/menu/action');
 $command_view = ViewFactory::create_view(IViewFactory::MESSAGE, 'widgets/menu/command');
 // Output Actions
 $items = array();
 foreach($actions as $action) {
 	// commands
-	$view = ($action instanceof ICommand) ? $command_view : $action_view; 
+	$view = ($action instanceof ICommand) ? $command_view : $action_view;
 	$view->assign('action', $action);
 	$view->assign('form_validation', $form_validation);
-	$items[$action->get_name()] = $view->render();
+	$items[] = array($action->get_name() => $view->render());
 }
 $c = count($items) - 1;
 $i = 0;
-foreach($items as $name => $item) {
-	$name = String::plain_ascii($name);
-	$cls = array($class);
-	if ($i === 0) {
-		$cls[] = "{$class}_first";
+foreach($items as $arr_item) {
+	foreach($arr_item as $name => $item) {
+		$name = String::plain_ascii($name);
+		$cls = array($class);
+		if ($i === 0) {
+			$cls[] = "{$class}_first";
+		}
+		if ($i === $c) {
+			$cls[] = "{$class}_last";
+		}
+		$cls[] = "{$class}_{$name}";
+		print html::tag('li', $item, array('class' => implode(' ', $cls)));
+		$i++;
 	}
-	if ($i === $c) {
-		$cls[] = "{$class}_last";
-	}
-	$cls[] = "{$class}_{$name}"; 
-	print html::tag('li', $item, array('class' => implode(' ', $cls)));
-	$i++;
 }
