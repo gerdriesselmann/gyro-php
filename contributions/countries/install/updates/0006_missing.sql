ab8f9d32f6c3f71a478a76c41f48c7aeae82e33a
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2011-12-18 23:17:55 +0000

    Added 4 countries founded in the last year

---
+++ contributions/countries/install/updates/0006_missing.sql
@@ -0,0 +1,11 @@
+INSERT INTO countries (id, id_continent, code3, codenum, name)  VALUES
+  ('CW', 'SA', 'CUW', 531, 'Curaçao'),
+  ('SX', 'SA', 'SXM', 534, 'Sint Maarten (Dutch part)'),
+  ('BQ', 'SA', 'BES', 535, 'Bonaire, Sint Eustatius and Saba'),
+  ('SS', 'AF', 'SSD', 728, 'South Sudan');
+
+INSERT INTO countriestranslations (id_country, lang, name) VALUES
+  ('CW', 'de', 'Curaçao'),
+  ('SX', 'de', 'Sint Marten (Holländischer Teil)'),
+  ('BQ', 'de', 'Bonaire, Sint Eustatius und Saba'),
+  ('SS', 'de', 'Süd-Sudan');
