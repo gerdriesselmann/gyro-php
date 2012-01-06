ca52dd88b820d2dbaff3e8265faff1f1ea0be3cc
gerd.riesselmann <gerd.riesselmann@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2012-01-06 21:30:46 +0000

    Use new Version constant

--- trunk/contributions/javascript.jquery/install/check_preconditions.php
+++ trunk/contributions/javascript.jquery/install/check_preconditions.php
@@ -2,7 +2,7 @@
 function javascript_jquery_check_preconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('javascript.jquery') . 'data/' . Config::get_value(ConfigJQuery::JQUERY_VERSION) . '/';
+	$root = Load::get_module_dir('javascript.jquery') . 'data/' . Config::get_value(ConfigJQuery::VERSION) . '/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js'), SystemUpdateInstaller::COPY_OVERWRITE));
 	return $ret;
 }
--- trunk/contributions/javascript.jqueryui/install/check_preconditions.php
+++ trunk/contributions/javascript.jqueryui/install/check_preconditions.php
@@ -2,7 +2,7 @@
 function javascript_jqueryui_check_preconditions() {
 	$ret = new Status();
 	Load::components('systemupdateinstaller');
-	$root = Load::get_module_dir('javascript.jqueryui') . 'data/' . Config::get_value(ConfigJQueryUI::JQUERYUI_VERSION) . '/';
+	$root = Load::get_module_dir('javascript.jqueryui') . 'data/' . Config::get_value(ConfigJQueryUI::VERSION) . '/';
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('js'), SystemUpdateInstaller::COPY_OVERWRITE));
 	$ret->merge(SystemUpdateInstaller::copy_to_webroot($root, array('css'), SystemUpdateInstaller::COPY_NO_REPLACE));
 	return $ret;
