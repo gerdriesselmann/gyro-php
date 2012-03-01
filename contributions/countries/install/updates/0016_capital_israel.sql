1dc9e6c0851c4dc7e27ad2cb02a19b967aeeb420
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2012-03-01 21:46:58 +0000

    Israel was missing a capital

---
+++ contributions/countries/install/updates/0016_capital_israel.sql
@@ -0,0 +1,2 @@
+UPDATE `countries` SET `capital` = 'Jerusalem', `lat_capital` =  '31.779167', `lon_capital` = '35.223611' WHERE `id` =  'IL';
+UPDATE `countriestranslations` SET `capital` = 'Jerusalem' WHERE `id_country` =  'IL';
