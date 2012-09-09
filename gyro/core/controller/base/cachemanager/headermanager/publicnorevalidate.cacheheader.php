a4a888316d3e31fcb6c8378663a26f6a95d9ba01
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2012-09-09 21:57:40 +0000

    Add cache headers without must-revalidate

--- gyro/core/controller/base/cachemanager/headermanager/publicnorevalidate.cacheheader.php
+++ gyro/core/controller/base/cachemanager/headermanager/publicnorevalidate.cacheheader.php
@@ -2,13 +2,13 @@
 /**
  * Allow client and proxy to cache, but force it to look up ressource on each request
  */
-class PublicRigidCacheHeaderManager extends BaseCacheHeaderManager {
+class PublicNoRevalidateCacheHeaderManager extends BaseCacheHeaderManager {
 	/**
 	 * Returns cache control header's content
 	 * 
 	 * @param timestamp $expirationdate
 	 */
 	protected function get_cache_control($expirationdate, $max_age) {
-		return "public, must-revalidate, max-age=0";
+		return "public, max-age=$max_age";
 	}
 }
