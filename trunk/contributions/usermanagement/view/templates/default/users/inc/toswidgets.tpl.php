4c96123389a282e5ee462880a0458469d9444ffc
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2013-02-04 12:45:23 +0000

    Allow to translate this in users translation, not only default

--- trunk/contributions/usermanagement/view/templates/default/users/inc/toswidgets.tpl.php
+++ trunk/contributions/usermanagement/view/templates/default/users/inc/toswidgets.tpl.php
@@ -2,4 +2,4 @@
 /**
  * You should modify this template due to the laws of your country!
  */
-print WidgetInput::output('tos', tr('I agree to the Terms of Service'), false, WidgetInput::CHECKBOX);
+print WidgetInput::output('tos', tr('I agree to the Terms of Service', array('users', 'default')), false, WidgetInput::CHECKBOX);
