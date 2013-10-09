25d7603bd6e7336d9a8531f532618b10fc4ed575
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2013-10-09 21:52:52 +0000

    Make filtertext work with buttons and make wrapper around several filters themeable

--- gyro/core/view/templates/default/widgets/filtertext.tpl.php
+++ gyro/core/view/templates/default/widgets/filtertext.tpl.php
@@ -1,18 +1,19 @@
 <?php
 $param = $adapter->get_param();
 $reset = $adapter->get_reset_param();
+$param_submit = $param . '_submit';
 ?>
 <div class="textfilter">
 	<form class="has_focus" name="frm_<?=$param?>" action="<?=$url_self?>" method="get">
 		<?php
 		foreach($page_data->get_get()->get_array() as $key => $v) {
-			if ($key != $param && $key != $reset) {
+			if ($key != $param && $key != $reset && $key != $param_submit) {
 				print WidgetInput::output($key, '', $v, WidgetInput::HIDDEN, array('id' => ''));
 			}		
 		}
 		?>
 		<?php print WidgetInput::output($param, tr('Filter by %title', array('app', 'core'), array('%title' => $title)), $value, WidgetInput::TEXT, array(), WidgetInput::NO_BREAK) ?>
-		<?php print WidgetInput::output($param . '_submit', '', tr('Filter', array('app', 'core')), WidgetInput::SUBMIT, array(), WidgetInput::NO_BREAK) ?>
+		<?php print WidgetInput::output($param_submit, '', tr('Filter', array('app', 'core')), WidgetInput::SUBMIT, array(), WidgetInput::NO_BREAK) ?>
 		<?php print WidgetInput::output($reset, '', tr('No filter', array('app', 'core')), WidgetInput::SUBMIT, array(), WidgetInput::NO_BREAK) ?>
 	</form>
 </div>	
