3430997c342cefc3f9f6c6fe587eb38935ffb646
Gerd Riesselmann <gerdriesselmann@users.noreply.github.com>
2013-11-16 00:49:42 +0000

    Add email type input widget

---
+++ gyro/core/view/widgets/input/email.input.widget.php
@@ -0,0 +1,11 @@
+<?php
+require_once GYRO_CORE_DIR . 'view/widgets/input/base/email.input.widget.php';
+
+/**
+ * A date widget
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup View
+ */
+class InputWidgetEMail extends InputWidgetEMailBase {
+}
