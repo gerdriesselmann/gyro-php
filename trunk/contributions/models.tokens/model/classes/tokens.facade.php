b44608f8c33c6b10a40267d3b4c218c0290a34bc
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2014-01-09 19:00:22 +0000

    Extend tokens by expirationdate

--- trunk/contributions/models.tokens/model/classes/tokens.facade.php
+++ trunk/contributions/models.tokens/model/classes/tokens.facade.php
@@ -6,10 +6,10 @@ class Tokens {
 	/**
 	 * Create a token for given context
 	 * 
-	 * @param string $context
 	 * @param string $seed Optional extra seed for token
+	 * @param int $duration_seconds Valid duration time in seconds. Defaults to 10 days
 	 */
-	public static function create_token($seed = false) {
+	public static function create_token($seed = false, $duration_seconds = 0) {
 		self::clear();
 		
 		$dao = new DAOTokens();
@@ -18,8 +18,13 @@ class Tokens {
 			$token = Common::create_token($seed);
 			$dao->token = $token;
 		} while ($dao->count() > 0);
-		
-		$cmd = CommandsFactory::create_command('tokens', 'create', array('token' => $token));
+
+		if ($duration_seconds == 0) { $duration_seconds = 10 * GyroDate::ONE_DAY; }
+
+		$cmd = CommandsFactory::create_command('tokens', 'create', array(
+			'token' => $token,
+			'expirationdate' => time() + $duration_seconds
+		));
 		$cmd->execute();
 		
 		return $token;
@@ -27,7 +32,24 @@ class Tokens {
 	
 	private static function clear() {
 		$dao = new DAOTokens();
-		$dao->add_where('creationdate', '<', time() - 10 * GyroDate::ONE_DAY);
+		$dao->add_where('expirationdate', '<', time());
 		$dao->delete(DataObjectBase::WHERE_ONLY);
 	}
+
+	public static function exists($token) {
+		$dao = new DAOTokens();
+		$dao->token = $token;
+		$dao->add_where('expirationdate', '>', time());
+
+		return $dao->count() > 0;
+	}
+
+	public static function remove($token) {
+		$dao = new DAOTokens();
+		$dao->token = $token;
+
+		$cmd = CommandsFactory::create_command($dao, 'delete', array());
+		return $cmd->execute();
+	}
+
 }
--- trunk/contributions/models.tokens/model/classes/tokens.model.php
+++ trunk/contributions/models.tokens/model/classes/tokens.model.php
@@ -3,6 +3,10 @@
  * Tokens DAO class
  */
 class DAOTokens extends DataObjectBase {
+	public $token;
+	public $expirationdate;
+	public $creationdate;
+
 	/**
 	 * Create the table object describing this dataobejcts table
 	 */
@@ -11,6 +15,7 @@ class DAOTokens extends DataObjectBase {
 			'tokens',
 			array(
 				new DBFieldText('token', 40, null, DBField::NOT_NULL),
+				new DBFieldDateTime('expirationdate', null, DBField::NOT_NULL),
 				new DBFieldDateTime('creationdate', DBFieldDateTime::NOW, DBFieldDateTime::TIMESTAMP | DBField::NOT_NULL)
 			),
 			'token'
