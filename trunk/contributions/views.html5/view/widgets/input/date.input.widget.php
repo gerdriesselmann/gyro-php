aad845db02a2616d8613ab5caf2da6a8990d3d63
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2014-04-15 14:02:34 +0000

    Format date in Y-m-d, since this is understood by Browsers

--- trunk/contributions/views.html5/view/widgets/input/date.input.widget.php
+++ trunk/contributions/views.html5/view/widgets/input/date.input.widget.php
@@ -9,6 +9,18 @@ require_once GYRO_CORE_DIR . 'view/widgets/input/base/date.input.widget.php';
  */
 class InputWidgetDate extends InputWidgetDateBase {
 	/**
+	 * Add new attributes or process old ones
+	 */
+	protected function extend_attributes(&$attrs, $params, $name, $title, $value, $policy) {
+		parent::extend_attributes($attrs, $params, $name, $title, $value, $policy);
+		if ($value) {
+			$value = date('Y-m-d', GyroDate::datetime($value));
+		}
+		$attrs['value'] = $value;
+		$attrs['class'] = trim(Arr::get_item($attrs, 'class', '') . ' date');
+	}
+
+	/**
 	 * Render the actual widget
 	 */
 	protected function render_input($attrs, $params, $name, $title, $value, $policy) {
