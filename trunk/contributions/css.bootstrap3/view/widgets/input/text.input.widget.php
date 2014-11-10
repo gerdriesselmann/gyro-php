982f0edf16bac5c23de2be7c32a84a5c870d1b30
gerd.riesselmann@gmail.com <gerd.riesselmann@gmail.com@1be89b24-e832-11de-8dea-4d08a2fd72f5>
2014-11-10 23:42:39 +0000

    Allow setting of input group addons

---
+++ trunk/contributions/css.bootstrap3/view/widgets/input/text.input.widget.php
@@ -0,0 +1,47 @@
+<?php
+require_once GYRO_CORE_DIR . 'view/widgets/input/base/text.input.widget.php';
+
+/**
+ * A text widget
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup View
+ */
+class InputWidgetText extends InputWidgetTextBase {
+    /**
+     * Render the actual widget
+     */
+    protected function render_input($attrs, $params, $name, $title, $value, $policy) {
+        $needs_div = false;
+
+        $text_box = html::input('text', $name, $attrs);
+        $pre = Arr::get_item($params, 'addon-pre');
+        if ($pre) {
+            $text_box = html::span(String::escape($pre), 'input-group-addon') . $text_box;
+            $needs_div = true;
+        }
+
+        $post = Arr::get_item($params, 'addon-post');
+        if ($post) {
+            $text_box = $text_box . html::span(String::escape($post), 'input-group-addon');
+            $needs_div = true;
+        }
+
+        if ($needs_div) {
+            $text_box = html::div($text_box, 'input-group');
+        }
+
+        return $text_box;
+    }
+
+    /**
+     * Create default attribute array
+     */
+    protected function create_default_attributes($params, $name, $policy) {
+        $attrs = parent::create_default_attributes($params, $name, $policy);
+
+        unset($attrs['addon-pre']);
+        unset($attrs['addon-post']);
+        return $attrs;
+    }
+}
