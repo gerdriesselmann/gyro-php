8154f6198dd6b60302d429e8595571c72b8dd072
Gerd Riesselmann <gerd@gerd-riesselmann.net>
2015-01-15 16:58:47 +0100

    Use charset and language from config, do not hard code

--- contributions/css.yaml/data/3.3/template/page.tpl.php
+++ contributions/css.yaml/data/3.3/template/page.tpl.php
@@ -6,7 +6,7 @@
  */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=Locale::get_language()?>" lang="<?=Locale::get_language()?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=<?=strtoupper(Locale::get_charset())?>" />
 	<meta name="language" content="<?=Locale::get_language()?>" />
