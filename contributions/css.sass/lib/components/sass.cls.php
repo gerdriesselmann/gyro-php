16982f1baca8ffd7f04b98485e64880186402668
Gerd Riesselmann <gerd@gerd-riesselmann.net>
2015-10-25 18:56:35 +0100

    Allow generation of all CSS into 1 directory

--- contributions/css.sass/lib/components/sass.cls.php
+++ contributions/css.sass/lib/components/sass.cls.php
@@ -16,8 +16,18 @@ class SASS {
 	 * @return Status
 	 */
 	public static function compile_file($file) {
-		$in_file = APP_INCLUDE_ABSPATH . 'view/sass/' . $file;
-		$out_file = APP_INCLUDE_ABSPATH . Config::get_value(ConfigSASS::OUTPUT_DIR) . $file;
+		$in_dir = APP_INCLUDE_ABSPATH . 'view/sass/';
+		$file = str_replace($in_dir, '', $file);
+		$in_file = $in_dir . $file;
+
+		$out_file = APP_INCLUDE_ABSPATH . Config::get_value(ConfigSASS::OUTPUT_DIR);
+		$out_base_file = str_replace('.scss', '.css', $file);
+		$out_base_file = str_replace('.sass', '.css', $out_base_file);
+		if (Config::has_feature(ConfigSASS::KEEP_DIRECTORY_STRUCTURE)) {
+			$out_file .= $out_base_file;
+		} else {
+			$out_file .= basename($out_base_file);
+		}
 		return self::run_cli_with($in_file, $out_file);
 	}
 
@@ -28,8 +38,32 @@ class SASS {
 	 */
 	public static function compile_all() {
 		$in_file = APP_INCLUDE_ABSPATH . 'view/sass/';
-		$out_file = APP_INCLUDE_ABSPATH . Config::get_value(ConfigSASS::OUTPUT_DIR);
-		return self::run_cli_with($in_file, $out_file);
+		if (Config::has_feature(ConfigSASS::KEEP_DIRECTORY_STRUCTURE)) {
+			$out_file = APP_INCLUDE_ABSPATH . Config::get_value(ConfigSASS::OUTPUT_DIR);
+			return self::run_cli_with($in_file, $out_file);
+		} else {
+			return self::compile_dir($in_file);
+		}
+	}
+
+	private static function compile_dir($dir_path) {
+		$err = new Status();
+		$it = new DirectoryIterator($dir_path);
+		foreach($it as $file_info) {
+			if (!$file_info->isDot()) {
+				if ($file_info->isDir()) {
+					$err->merge(self::compile_dir($file_info->getPathname()));
+				} else {
+					switch ($file_info->getExtension()) {
+						case 'scss':
+						case 'sass':
+							$err->merge(self::compile_file($file_info->getPathname()));
+							break;
+					}
+				}
+			}
+		}
+		return $err;
 	}
 
 	/**
@@ -39,7 +73,7 @@ class SASS {
 	 * @return Status
 	 */
 	private static function run_cli_with($in_file, $out_file) {
-		$elems = array('sass', '--update', escapeshellarg($in_file . ':' . $out_file));
+		$elems = array('sass', '-f', '--update', escapeshellarg($in_file . ':' . $out_file));
 		$cli = implode(' ', $elems);
 
 		Load::commands('generics/execute.shell');
--- contributions/css.sass/start.inc.php
+++ contributions/css.sass/start.inc.php
@@ -21,8 +21,21 @@ class ConfigSASS {
 	 * "www/css/generated"
 	 */
 	const OUTPUT_DIR = 'SASS_OUTPUT_DIR';
+
+	/**
+	 * If generator should keep directory structure (default) or not.
+	 *
+	 * If true file app/view/sass/lib/elements.sass would be compiled to
+	 *    www/css/generated/lib/elements.css
+	 *
+	 * If false the target would be
+	 *    www/css/generated/elements.css
+	 */
+	const KEEP_DIRECTORY_STRUCTURE = 'SASS_KEEP_DIRECTORY_STRUCTURE';
+
 }
 
 Config::set_value_from_constant(ConfigSASS::OUTPUT_DIR, 'APP_SASS_OUTPUT_DIR', 'www/css/generated/');
+Config::set_feature_from_constant(ConfigSASS::KEEP_DIRECTORY_STRUCTURE, 'APP_SASS_KEEP_DIRECTORY_STRUCTURE', true);
 
 
