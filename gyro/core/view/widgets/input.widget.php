cb21f7b78b1dbfe0b6158eca552ddf30e8bbd805
Gerd Riesselmann <gr@audisto.com>
2020-01-20 16:39:01 +0100

    #28 Extending widgets by floating point text box, which does localization of floating point numbers

--- gyro/core/view/widgets/input.widget.php
+++ gyro/core/view/widgets/input.widget.php
@@ -83,8 +83,9 @@ class WidgetInput implements IWidget {
 	const DATE = 'date';
 	const DATETIME = 'datetime';
 	const EMAIL = 'email';
-	const NUMBER = 'number';
+	const NUMBER = 'number'; // Actually an integer...
 	const PHONE = 'phone';
+	const FLOAT = 'float';
 
 	public static function output($name, $label, $value = '', $type = self::TEXT, $params = array(), $policy = self::NONE) {
 		$widget = new WidgetInput($name, $label, $value, $type, $params);
---
+++ gyro/core/view/widgets/input/float.input.widget.php
@@ -0,0 +1,11 @@
+<?php
+require_once GYRO_CORE_DIR . 'view/widgets/input/base/float.input.widget.php';
+
+/**
+ * A text widget for floating point values
+ * 
+ * @author Gerd Riesselmann
+ * @ingroup View
+ */
+class InputWidgetFloat extends InputWidgetFloatBase {
+}
