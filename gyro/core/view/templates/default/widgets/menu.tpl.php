1fc4006a058494a0af0092a334c8f866e2af8b75
Gerd Riesselmann <gr@audisto.com>
2022-11-02 15:07:03 +0100

    $page_data is not known here. Replace with null.

--- gyro/core/view/templates/default/widgets/menu.tpl.php
+++ gyro/core/view/templates/default/widgets/menu.tpl.php
@@ -2,7 +2,7 @@
 if (count($actions) || count($commands)) {
 	$out = '';
 	$view = ViewFactory::create_view(IViewFactory::MESSAGE, 'widgets/menu/items');
-	$view->assign('form_validation', $form_validation);
+	$view->assign('form_validation', isset($form_validation) ? $form_validation : '');
 
 	$view->assign('actions', $actions);
 	$view->assign('class', 'actions');
@@ -14,6 +14,6 @@ if (count($actions) || count($commands)) {
 
 	$view = ViewFactory::create_view(IViewFactory::MESSAGE, 'widgets/menu/group');
 	$view->assign('content', $out);
-	$view->assign('css_class', $css_class);
+	$view->assign('css_class', isset($css_class) ? $css_class : '');
 	print $view->render();
 }
