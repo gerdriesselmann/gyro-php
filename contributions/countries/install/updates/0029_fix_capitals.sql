9071236e7e2db984e2adeeed51dad533661abde1
Gerd Riesselmann <gr@audisto.com>
2026-07-03 10:25:26 +0200

    Switch test API from restcountries.com to countries.dev, and fix problems in data detected through test

---
+++ contributions/countries/install/updates/0029_fix_capitals.sql
@@ -0,0 +1,17 @@
+UPDATE countries SET capital = 'Gitega' WHERE id = 'BI';
+UPDATE countries SET capital = 'Vatican City' WHERE id = 'VA';
+UPDATE countries SET capital = 'Nur-Sultan' WHERE id = 'KZ';
+UPDATE countries SET capital = 'Sri Jayawardenepura Kotte' WHERE id = 'LK';
+UPDATE countries SET capital = 'Mbabane' WHERE id = 'SZ';
+UPDATE countries SET capital = 'Kyiv' WHERE id = 'UA';
+UPDATE countries SET capital = 'Saint John''s' WHERE id = 'AG';
+UPDATE countries SET capital = 'Papeetē' WHERE id = 'PF';
+UPDATE countries SET capital = 'City of Victoria' WHERE id = 'HK';
+UPDATE countries SET capital = 'Reykjavík' WHERE id = 'IS';
+UPDATE countries SET capital = 'Chișinău' WHERE id = 'MD';
+UPDATE countries SET capital = 'Nouméa' WHERE id = 'NC';
+UPDATE countries SET capital = 'City of San Marino' WHERE id = 'SM';
+UPDATE countries SET capital = 'Washington, D.C.' WHERE id = 'US';
+
+
+UPDATE countries SET code3 = 'UNK' WHERE id = 'XK';
